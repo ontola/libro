e137b640caa9eba98d4749cdff8d117d027144c9
Tom Dalenberg <tomdalenberg@argu.co>
2016-06-02 13:04:36 +0200

    Added tests with mocha, chai and enzyme

---
+++ tools/testhelper.js
@@ -0,0 +1,25 @@
+require('babel-register')();
+
+function ignoreStyles() {
+  return null;
+}
+
+var jsdom = require('jsdom').jsdom;
+var exposedProperties = ['window', 'navigator', 'document'];
+
+require.extensions['.scss'] = ignoreStyles;
+global.document = jsdom('');
+global.window = document.defaultView;
+
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+documentRef = document;
