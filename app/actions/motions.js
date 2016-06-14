e9ecd8a2cb2f41dc5aaf6837f1d24e6025ba5627
Tom Dalenberg <tomdalenberg@argu.co>
2016-06-14 16:56:08 +0200

    prepared votes in redux

--- app/actions/motions.js
+++ app/actions/motions.js
@@ -16,5 +16,5 @@ const apiGetMotions = (id) => ({
 });
 
 export {
-  apiGetMotions
+  apiGetMotions,
 };
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import motions from './motions';
+import votes from './votes';
 
 const rootReducer = combineReducers({
   motions,
+  votes,
   routing: routerReducer,
 });
 
