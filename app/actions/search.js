f57dcd4f2ffe7c6a751e8363845dbf4d3c456a84
Tom Dalenberg <tomdalenberg@argu.co>
2016-06-23 18:26:00 +0200

    fixed linting error

--- app/actions/search.js
+++ app/actions/search.js
@@ -1,22 +1,25 @@
 import { createAction } from 'redux-actions';
 import { CALL_API } from 'redux-api-middleware';
 import { ELASTICSEARCH_DOCUMENT_BASE } from '../constants/config';
-import * as action from '../constants/actionTypes';
+import * as actions from '../constants/actionTypes';
 
 const apiGetDocument = (id) => ({
   [CALL_API]: {
     endpoint: ELASTICSEARCH_DOCUMENT_BASE + id,
     method: 'GET',
     types: [
-      action.FETCH_DOCUMENT_REQUEST,
-      action.FETCH_DOCUMENT_SUCCESS,
-      action.FETCH_DOCUMENT_FAILURE,
+      actions.FETCH_DOCUMENT_REQUEST,
+      {
+        type: actions.FETCH_DOCUMENT_SUCCESS,
+        payload: (action, state, res) => res.json().then(({ _source: source }) => source),
+      },
+      actions.FETCH_DOCUMENT_FAILURE,
     ],
   },
 });
 
-const toggleDrawer = createAction(action.TOGGLE_DRAWER);
-const setHitCount = createAction(action.SET_HIT_COUNT);
+const toggleDrawer = createAction(actions.TOGGLE_DRAWER);
+const setHitCount = createAction(actions.SET_HIT_COUNT);
 
 export {
   apiGetDocument,
