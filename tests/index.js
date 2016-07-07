69a30d48354ec6b7ca7a4f9f4af9b6e32a4298a0
Tom Dalenberg <tomdalenberg@argu.co>
2016-07-07 15:29:54 +0200

    [IMP] Added a new e2e test for motions page - WIP

--- tests/index.js
+++ tests/index.js
@@ -1,10 +1,12 @@
 const DELAY = 1000;
 
 module.exports = {
-  'Test website': (browser) => {
+  'Test homepage': (browser) => {
     browser
       .url('http://localhost:3000')
       .waitForElementVisible('body', DELAY)
+      .assert.containsText('.Box__content p',
+        'Lorem ipsum')
       .end();
   },
 };
---
+++ tests/motions.js
@@ -0,0 +1,12 @@
+const DELAY = 1000;
+
+module.exports = {
+  'Go to a motion': (browser) => {
+    browser
+      .url('http://localhost:3000/motions')
+      .waitForElementVisible('body', DELAY)
+      .pause(DELAY)
+      .waitForElementVisible('.motions__list__item', DELAY)
+      .end();
+  },
+};
