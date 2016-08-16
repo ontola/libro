86571ed9d6684776a1576391291642c4c9ebeb7c
Fletcher91 <thom@argu.co>
2016-08-16 14:53:48 +0200

    [IMP] Docker image testing on CI
    We now run the nightwatch index test on the image with selenium to ensure we get a rendered page before we consider the build successful

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,7 @@ const DELAY = 1000;
 module.exports = {
   'Test homepage': (browser) => {
     browser
-      .url('http://localhost:3001')
+      .url(`${browser.launch_url}/`)
       .waitForElementVisible('body', DELAY)
       .assert.containsText('.Box__content p',
         'Lorem ipsum')
--- tests/motions.js
+++ tests/motions.js
@@ -3,7 +3,7 @@ const DELAY = 1000;
 module.exports = {
   'Go to a motion': (browser) => {
     browser
-      .url('http://localhost:3001/motions')
+      .url(`${browser.launch_url}/motions`)
       .waitForElementVisible('body', DELAY)
       .pause(DELAY)
       .waitForElementVisible('.motions__list__item', DELAY)
