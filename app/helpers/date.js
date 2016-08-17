569e65c7db360eb9770809c73510c6986e78b6b2
Tom Dalenberg <tomdalenberg@argu.co>
2016-08-17 14:52:11 +0200

    [IMP] Added documentation for date helper

--- app/helpers/date.js
+++ app/helpers/date.js
@@ -1,8 +1,23 @@
 import moment from 'moment';
 
+/**
+ * @constant
+ * @type {number}
+ */
 const MILLISECONDS_TO_SECONDS = 1000;
+
+
+/**
+ * @constant
+ * @type {string}
+ */
 const dateDisplayFormat = 'DD-MM-YY';
 
+
+/**
+ * @param {number} timestamp A unix timestamp
+ * @return {string} A formatted date string
+ */
 const formatDate = (timestamp) => {
   if (!timestamp) {
     return undefined;
