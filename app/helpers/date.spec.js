39d2243fc97061a8838982ecb4eaa0b213c4d8f1
Tom Dalenberg <tomdalenberg@argu.co>
2016-08-19 17:47:58 +0200

    [FIX] Date tests

--- app/helpers/date.spec.js
+++ app/helpers/date.spec.js
@@ -1,8 +1,8 @@
 import { assert } from 'chai';
 import { formatDate } from './date';
 
-const TIMESTAMP = 1457172000;
-const expectedOutput = '05-03-16';
+const TIMESTAMP = 1457172000000;
+const expectedOutput = '05-03-2016';
 
 describe('Date', () => {
   it('format correctly', () => {
