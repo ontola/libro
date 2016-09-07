1b5c2e94c9fb22e2fc62039bdd760b18a23871cf
Joep Meindertsma <joep@argu.co>
2016-09-07 13:13:05 +0200

    [FEATURE] Attachment component

--- app/components/Attachment/index.jsx
+++ app/components/Attachment/index.jsx
@@ -1,32 +1,52 @@
 // @flow
-// import './Attachment.scss';
+import './Attachment.scss';
 import React, { PropTypes } from 'react';
+import FontAwesome from 'react-fontawesome';
 
 const propTypes = {
   title: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
+  percentageDownloaded: PropTypes.number,
+  hasPreview: PropTypes.bool,
+  isDownloaded: PropTypes.bool,
+  isDownloading: PropTypes.bool,
 };
 
-const EventItem = ({
+const Attachment = ({
   title,
+  hasPreview,
+  isDownloaded,
+  isDownloading,
+  percentageDownloaded,
   url,
 }) => {
+  const fillStyle = {
+    width: `${percentageDownloaded}%`,
+  };
+
   return (
-    <div className="EventItem">
-      {showIndex && !isCurrent && indexComponent}
-      <Collapsible
-        trigger={
-          <Heading
-            size="3"
-            children={title}
-          />}
-        visibleContent={detailsBar}
-        children={content}
-      />
-      {progress}
+    <div className="Attachment" href={url}>
+      <a href={url} className="Attachment__primary" target="_blank" >
+        <FontAwesome className="Attachment__icon" name="file" />
+        <div className="Attachment__text">
+          {title}
+        </div>
+        <div className="Attachment__fill" style={fillStyle} />
+      </a>
+      {(!isDownloaded && !isDownloading) &&
+        <a href="#" className="Attachment__inside-button">
+          <FontAwesome className="Attachment__icon" name="download" />
+        </a>
+      }
+      {hasPreview &&
+        <a href="#" className="Attachment__inside-button">
+          <FontAwesome className="Attachment__icon" name="search-plus" />
+        </a>
+      }
     </div>
   );
 };
 
-export default EventItem;
-EventItem.propTypes = propTypes;
+Attachment.propTypes = propTypes;
+
+export default Attachment;
