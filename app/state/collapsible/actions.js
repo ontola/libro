1005254c2b8dd0cc050c0ba3c7b9325a63c7cbf9
Tom Dalenberg <tomdalenberg@argu.co>
2016-10-20 14:13:09 +0200

    [IMP] Cleaned up some files

--- app/state/collapsible/actions.js
+++ app/state/collapsible/actions.js
@@ -5,11 +5,4 @@ import { createAction } from 'redux-actions';
 export const toggleOne = createAction(actions.COLL_TOGGLE_ONE);
 export const toggleAll = createAction(actions.COLL_TOGGLE_GROUP);
 
-// export const toggleAll = (group) => ({
-//   type: actions.COLL_TOGGLE_GROUP,
-//   payload: {
-//     group,
-//   },
-// });
-
 export const initializeCollapsible = createAction(actions.COLL_ADD);
