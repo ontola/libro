825886a63365b3e3bf6d807bf97ae9bbd0732635
Joep Meindertsma <joep@argu.co>
2016-12-11 10:56:14 +0100

    [IMP] Show argument text in list item, static motion Ids, bigger VoteMatch

--- app/containers/MotionsContainer.jsx
+++ app/containers/MotionsContainer.jsx
@@ -8,7 +8,7 @@ import { fetchMotions } from 'state/motions/actions';
 import MotionContainer from 'containers/MotionContainer';
 
 const propTypes = {
-  motions: PropTypes.object,
+  // motions: PropTypes.object,
   loadMotions: PropTypes.func.isRequired,
 };
 
@@ -30,7 +30,15 @@ class MotionsContainer extends Component {
   }
 
   render() {
-    const { motions } = this.props;
+    // const { motions } = this.props;
+    const motions = [
+      '6117dd10-2cf8-e511-9672-e4115babb880',
+      '169b7429-14f8-e511-9672-e4115babb880',
+      '2ea244f5-14f8-e511-9672-e4115babb880',
+      '6717dd10-2cf8-e511-9672-e4115babb880',
+      'c0e2a617-79f2-e511-9672-e4115babb880',
+      '3137bf58-89f5-e511-9672-e4115babb880',
+    ];
     return motions.size > 0 && <List renderItem={renderMotionContainer} items={motions} />;
   }
 }
