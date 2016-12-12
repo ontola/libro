557417b5b7ebb56af784c994a40a1d79fbede1cb
Fletcher91 <thom@argu.co>
2016-12-12 14:47:40 +0100

    [FIX] Motions#index working [HOTFIX] Return existing model if it has a title

--- app/containers/MotionsContainer.jsx
+++ app/containers/MotionsContainer.jsx
@@ -32,14 +32,14 @@ class MotionsContainer extends Component {
   render() {
     // const { motions } = this.props;
     const motions = [
-      '6117dd10-2cf8-e511-9672-e4115babb880',
-      '169b7429-14f8-e511-9672-e4115babb880',
-      '2ea244f5-14f8-e511-9672-e4115babb880',
-      '6717dd10-2cf8-e511-9672-e4115babb880',
-      'c0e2a617-79f2-e511-9672-e4115babb880',
-      '3137bf58-89f5-e511-9672-e4115babb880',
+      { id: '6117dd10-2cf8-e511-9672-e4115babb880' },
+      { id: '169b7429-14f8-e511-9672-e4115babb880' },
+      { id: '2ea244f5-14f8-e511-9672-e4115babb880' },
+      { id: '6717dd10-2cf8-e511-9672-e4115babb880' },
+      { id: 'c0e2a617-79f2-e511-9672-e4115babb880' },
+      { id: '3137bf58-89f5-e511-9672-e4115babb880' },
     ];
-    return motions.size > 0 && <List renderItem={renderMotionContainer} items={motions} />;
+    return motions.length > 0 && <List renderItem={renderMotionContainer} items={motions} />;
   }
 }
 
