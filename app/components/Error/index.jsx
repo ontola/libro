2f770c2e07e39e1284b50ec9c5871b2f386acc29
Joep Meindertsma <joepmeindertsma@gmail.com>
2017-11-22 14:11:09 +0100

    [AOD-120] Loading indicators for Link components

--- app/components/Error/index.jsx
+++ app/components/Error/index.jsx
@@ -1,29 +1,7 @@
-import HttpStatus from 'http-status-codes';
-import { getP } from 'link-lib';
-import PropTypes from 'prop-types';
 import React from 'react';
 
-const propTypes = {
-  data: PropTypes.shape({
-    'http://www.w3.org/2011/http#statusCodeValue': PropTypes.number,
-  }),
-};
-
-function getErrorMessage(code) {
-  switch (code) {
-    case HttpStatus.NOT_FOUND:
-      return 'Object could not be found';
-    default:
-      return 'An unknown error occurred';
-  }
-}
-
-const Error = props => (
-  <p>
-    {getErrorMessage(getP(props.data, 'http://www.w3.org/2011/http#statusCodeValue'))}
-  </p>
+const Error = () => (
+  <p>An unknown error occurred, please try again later.</p>
 );
-
-Error.propTypes = propTypes;
 
 export default Error;
--- app/components/Loading/index.jsx
+++ app/components/Loading/index.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
-const Loading = () => <span>Loading...</span>;
+import './Loading.scss';
 
-export default Loading;
+import {
+  Card,
+  CardRow,
+  CardContent,
+} from '../../components';
+
+const LoadingText = () => <span>Loading...</span>;
+
+export const LoadingSideBarLink = () => (
+  <div className="LoadingSideBarLink">
+    <div className="LoadingSideBarLink__content Loading__background Loading__background--inverse" />
+  </div>
+);
+
+export const LoadingCard = () => (
+  <Card>
+    <CardContent>
+      <div className="Loading__Header Loading__background" />
+      <div className="Loading__Paragraph Loading__background" />
+      <div className="Loading__Paragraph Loading__background" />
+      <div className="Loading__Paragraph--shorter Loading__background" />
+    </CardContent>
+  </Card>
+);
+
+export const LoadingRow = () => (
+  <CardRow>
+    <CardContent>
+      <div className="Loading__Paragraph Loading__background" />
+    </CardContent>
+  </CardRow>
+);
+
+export default LoadingText;
