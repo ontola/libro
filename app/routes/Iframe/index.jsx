2297409a72b8cd3b73970e73aa1b7edf4335f693
Fletcher91 <thom@argu.co>
2017-11-22 12:15:14 +0100

    [AOD-117] Iframe loading indicator

--- app/routes/Iframe/index.jsx
+++ app/routes/Iframe/index.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 
@@ -9,25 +10,68 @@ const propTypes = {
 };
 
 const identifier = 'iframe';
+const loaderStyle = {
+  left: '1em',
+  position: 'absolute',
+  top: '1em',
+  zIndex: 1,
+};
 
-const Iframe = ({ location, title }) => (
-  <div>
-    <Helmet title={title} />
-    <iframe
-      id={identifier}
-      referrerPolicy="no-referrer-when-downgrade"
-      src={location}
-      style={{
-        height: '100%',
-        position: 'absolute',
-        width: '100%',
-      }}
-      title="Argu contents"
-    >
-      Oeps! Jouw browser ondersteunt geen iframe elementen. Probeer een andere browser.
-    </iframe>
-  </div>
-);
+class Iframe extends Component {
+  constructor(props) {
+    super(props);
+    this.hideLoader = this.hideLoader.bind(this);
+    this.state = {
+      loading: false,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location !== nextProps.location) {
+      this.showLoader();
+    }
+  }
+
+  hideLoader() {
+    this.setState({
+      loading: false,
+    });
+  }
+
+  loadComponent() {
+    return this.state.loading && <FontAwesome spin name="spinner" style={loaderStyle} />;
+  }
+
+  showLoader() {
+    this.setState({
+      loading: true,
+    });
+  }
+
+  render() {
+    const { location, title } = this.props;
+    return (
+      <div>
+        <Helmet title={title} />
+        {this.loadComponent()}
+        <iframe
+          id={identifier}
+          referrerPolicy="no-referrer-when-downgrade"
+          src={location}
+          style={{
+            height: '100%',
+            position: 'absolute',
+            width: '100%',
+          }}
+          title="Argu contents"
+          onLoad={this.hideLoader}
+        >
+          Oeps! Jouw browser ondersteunt geen iframe elementen. Probeer een andere browser.
+        </iframe>
+      </div>
+    );
+  }
+}
 
 Iframe.propTypes = propTypes;
 
