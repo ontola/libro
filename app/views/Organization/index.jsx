f38633b9097ffe84a2e929f2526d765c5d31e0eb
Fletcher91 <thom@argu.co>
2017-11-28 17:28:57 +0100

    [AOD-110] Remove superfluous items from sidebar

--- app/views/Organization/index.jsx
+++ app/views/Organization/index.jsx
@@ -1,38 +1,15 @@
 import { RENDER_CLASS_NAME } from 'link-lib';
 import { Property } from 'link-redux';
-import PropTypes from 'prop-types';
 import React from 'react';
 
-import {
-  SideBarLink,
-} from '../../components';
 import LinkedRenderStore, { NS } from '../../helpers/LinkedRenderStore';
-import path from '../../helpers/paths';
 
 import './NavBarContent.scss';
 import './properties/name';
 
-const propTypes = {
-  voteMatchCount: PropTypes.number,
-};
-
-const OrganizationSidebarBlock = ({
-  voteMatchCount,
-}) => (
-  <div>
-    <SideBarLink icon="search" label="Moties zoeken" to={path.search()} />
-    <SideBarLink
-      count={voteMatchCount}
-      icon="compass"
-      label="Stemwijzer maken"
-      to={path.createVoteMatch()}
-    />
-    <SideBarLink isIndex icon="th-large" label="Overzicht" to={path.index()} />
-    <Property label={NS.argu('navigationsMenu')} topology={NS.argu('sidebar')} />
-  </div>
+const OrganizationSidebarBlock = () => (
+  <Property label={NS.argu('navigationsMenu')} topology={NS.argu('sidebar')} />
 );
-
-OrganizationSidebarBlock.propTypes = propTypes;
 
 LinkedRenderStore.registerRenderer(
   OrganizationSidebarBlock,
