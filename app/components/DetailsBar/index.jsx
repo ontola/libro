b7e07355f58c217617ab8cd2e14d5cc311c9acac
Fletcher91 <thom@argu.co>
2018-02-10 17:12:47 +0100

    [AOD-110] Update `DetailsBar` to use the TopologyProvider API

--- app/components/DetailsBar/index.jsx
+++ app/components/DetailsBar/index.jsx
@@ -1,33 +1,16 @@
-/* eslint class-methods-use-this: 0 */
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { TopologyProvider } from 'link-redux';
 
 import { NS } from '../../helpers/LinkedRenderStore';
 
 import './DetailsBar.scss';
 
-class DetailsBar extends Component {
-  getChildContext() {
-    return {
-      topology: NS.argu('detail'),
-    };
-  }
+class DetailsBar extends TopologyProvider {
+  constructor() {
+    super();
 
-  render() {
-    return (
-      <div className="DetailsBar">
-        {this.props.children}
-      </div>
-    );
+    this.className = 'DetailsBar';
+    this.topology = NS.argu('detail');
   }
 }
-
-DetailsBar.childContextTypes = {
-  topology: PropTypes.object,
-};
-
-DetailsBar.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default DetailsBar;
