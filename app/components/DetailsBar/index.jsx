065b5683b128ab4e38f7ecc1d9711f2feeee52f9
Jurrian Tromp <jurriantromp@gmail.com>
2018-04-11 12:25:49 -0300

    [AOD-81] DetailBar with right-side functionality

--- app/components/DetailsBar/index.jsx
+++ app/components/DetailsBar/index.jsx
@@ -1,4 +1,5 @@
 import { TopologyProvider } from 'link-redux';
+import React from 'react';
 
 import { NS } from '../../helpers/LinkedRenderStore';
 
@@ -8,8 +9,18 @@ class DetailsBar extends TopologyProvider {
   constructor() {
     super();
 
-    this.className = 'DetailsBar';
     this.topology = NS.argu('detail');
+  }
+
+  render() {
+    return (
+      <div className="DetailsBar">
+        {this.props.children}
+        <div className="DetailsBar__right">
+          {this.props.right}
+        </div>
+      </div>
+    );
   }
 }
 
