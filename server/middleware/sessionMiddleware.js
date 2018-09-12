2f0dceb3c975c72ab2b7b27ed26082d6c8e9ec0d
Fletcher91 <thom@argu.co>
2018-09-12 17:33:26 +0200

    [AOD-188] Log redis connection information

--- server/middleware/sessionMiddleware.js
+++ server/middleware/sessionMiddleware.js
@@ -2,9 +2,22 @@ import connectRedis from 'connect-redis';
 import session from 'express-session';
 import Redis from 'ioredis';
 
-import { redisAddress, sessionSecret } from '../config';
+import { logLevel, redisAddress, sessionSecret } from '../config';
 
 const client = new Redis(redisAddress);
+
+if (logLevel === 'debug') {
+  client
+    .monitor()
+    .then((monitor) => {
+      monitor.on('monitor', (time, args, source, database) => {
+        if (database === '0') {
+          // eslint-disable-next-line no-console
+          console.debug(`[REDIS] ${time}: '${database}', '${source}', '${args.join(',')}'`);
+        }
+      });
+    });
+}
 
 const RedisStore = connectRedis(session);
 const redisStore = new RedisStore({
