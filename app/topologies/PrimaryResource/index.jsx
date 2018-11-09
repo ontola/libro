0ad60be3789049cc17eec7c04c60884e6d757477
Arthur Dingemans <arthur@argu.co>
2018-11-09 09:09:11 +0100

    [AOD-267] Add displayName to PrimaryResource

--- app/topologies/PrimaryResource/index.jsx
+++ app/topologies/PrimaryResource/index.jsx
@@ -5,6 +5,8 @@ import Topology from '../Topology';
 export const primaryResourceTopology = null;
 
 class PrimaryResource extends Topology {
+  static displayName = 'PrimaryResource';
+
   static propTypes = {
     children: PropTypes.node.isRequired,
   };
