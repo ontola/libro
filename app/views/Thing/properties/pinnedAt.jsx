ae06df8080ad88c5300192e7959e5439c8aabd9a
Fletcher91 <thom@argu.co>
2019-02-14 13:31:50 +0100

    [AOD-110] Fix pinnedAt label

--- app/views/Thing/properties/pinnedAt.jsx
+++ app/views/Thing/properties/pinnedAt.jsx
@@ -1,10 +1,17 @@
 import { linkedPropType, register } from 'link-redux';
 import React from 'react';
-import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import { defineMessages, injectIntl, intlShape } from 'react-intl';
 
 import Detail from '../../../components/Detail';
 import { NS } from '../../../helpers/LinkedRenderStore';
 import { detailsBarTopology } from '../../../topologies/DetailsBar';
+
+const messages = defineMessages({
+  pinnedAtLabel: {
+    defaultMessage: 'Pinned at {date}',
+    id: 'https://app.argu.co/i18n/pinnable/states/pinned/pinnedAtLabel',
+  },
+});
 
 class PinnedAt extends React.PureComponent {
   static type = NS.schema('Thing');
@@ -21,19 +28,14 @@ class PinnedAt extends React.PureComponent {
   };
 
   render() {
-    const { intl: { formatDate }, linkedProp } = this.props;
+    const { intl: { formatDate, formatMessage }, linkedProp } = this.props;
 
     return (
       <Detail
         icon="thumb-tack"
-        title={(
-          <FormattedMessage
-            defaultMessage="Pinned at {date}"
-            id="https://app.argu.co/i18n/pinnable/states/pinned/pinnedAtLabel"
-            values={{
-              date: formatDate(new Date(linkedProp.value)),
-            }}
-          />
+        title={formatMessage(
+          messages.pinnedAtLabel,
+          { date: formatDate(new Date(linkedProp.value)) }
         )}
       />
     );
