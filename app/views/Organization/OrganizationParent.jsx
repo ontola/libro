fa653f2149cb11ea290f5201d48244233e56029a
Fletcher91 <thom@argu.co>
2019-03-18 09:32:31 +0100

    [AOD-406] Revert "Make breadcrumbs less prominent"
    This reverts commit 8d41ae88

--- app/views/Organization/OrganizationParent.jsx
+++ app/views/Organization/OrganizationParent.jsx
@@ -1,9 +1,12 @@
 import {
+  linkType,
+  Property,
   register,
 } from 'link-redux';
 import React from 'react';
 
 import { NS } from '../../helpers/LinkedRenderStore';
+import { Breadcrumb } from '../../components/Breadcrumbs/index';
 import { parentTopology } from '../../topologies/Parent';
 
 class OrganizationParent extends React.PureComponent {
@@ -11,8 +14,24 @@ class OrganizationParent extends React.PureComponent {
 
   static topology = parentTopology;
 
+  static mapDataToProps = [NS.schema('name')];
+
+  static propTypes = {
+    name: linkType,
+  };
+
   render() {
-    return null;
+    return (
+      <React.Fragment>
+        <Property label={NS.schema('isPartOf')} />
+        <Breadcrumb
+          data-test="Thing-parent"
+          image={<Property label={NS.schema('image')} />}
+          label={<Property label={NS.schema('name')} />}
+          title={this.props.name.value}
+        />
+      </React.Fragment>
+    );
   }
 }
 
