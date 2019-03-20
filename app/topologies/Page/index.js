a076df0eee44274b1d97b9b9157e22c174bbcf71
Fletcher91 <thom@argu.co>
2019-03-20 18:08:10 +0100

    [AOD-433] Remove 1px padding on Page class

--- app/topologies/Page/index.js
+++ app/topologies/Page/index.js
@@ -1,8 +1,6 @@
 import { NS } from '../../helpers/LinkedRenderStore';
 import Topology from '../Topology';
 
-import './Page.scss';
-
 export const pageTopology = NS.argu('page');
 
 export class Page extends Topology {
