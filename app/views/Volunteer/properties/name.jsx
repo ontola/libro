b44b0220683adb0eddd9859880292a3a9eb03632
Arthur Dingemans <arthur@argu.co>
2019-06-19 09:44:01 +0200

    [AOD][TGL-13] Don't use Volunteer#name renderer in Select topology
    We don't want it to create a link

--- app/views/Volunteer/properties/name.jsx
+++ app/views/Volunteer/properties/name.jsx
@@ -4,23 +4,24 @@ import React from 'react';
 
 import { Heading, LDLink } from '../../../components';
 import { NS } from '../../../helpers/LinkedRenderStore';
-import { allTopologies } from '../../../topologies';
+import { allTopologiesExcept } from '../../../topologies';
+import { selectTopology } from '../../../topologies/Select';
 
 const propTypes = {
   linkedProp: linkedPropType,
 };
 
-const Email = ({ linkedProp }) => (
+const Name = ({ linkedProp }) => (
   <LDLink>
     <Heading size={3}>{linkedProp.value}</Heading>
   </LDLink>
 );
 
-Email.propTypes = propTypes;
+Name.propTypes = propTypes;
 
 export default LinkedRenderStore.registerRenderer(
-  Email,
+  Name,
   NS.teamGL('Volunteer'),
   NS.schema('name'),
-  allTopologies
+  allTopologiesExcept(selectTopology)
 );
