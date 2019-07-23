dae6c952ed8bdc2626c64c2ea48cafd8af6fda3f
Arthur Dingemans <arthur@argu.co>
2019-07-23 15:47:53 +0200

    [AOD][TGL-24] Refactor deltas
    Add supplant and change replace behaviour

--- app/helpers/arguDeltaProcessor.ts
+++ app/helpers/arguDeltaProcessor.ts
@@ -17,7 +17,7 @@ function processRemove(delta: Quadruple[], lrs: LinkReduxLRSType) {
 
 function processReplace(delta: Quadruple[], lrs: LinkReduxLRSType) {
   const replaceables = delta
-      .filter(([s, p, , g]) => g === lrs.namespaces.ontola('replace') && lrs.store.anyStatementMatching(s, p));
+      .filter(([s, , , g]) => g === lrs.namespaces.ontola('replace') && lrs.store.anyStatementMatching(s));
 
   return lrs.store.replaceMatches(replaceables);
 }
@@ -26,14 +26,35 @@ function processInvalidate(delta: Quadruple[], lrs: LinkReduxLRSType) {
   delta
       .filter(([, , , why]) => why === lrs.namespaces.ontola('invalidate'))
       .forEach(([s, p, o]) => {
-        lrs.store.match(
-            s === lrs.namespaces.sp('Variable') ? null : s,
-            p === lrs.namespaces.sp('Variable') ? null : p,
-            o === lrs.namespaces.sp('Variable') ? null : o,
-        ).forEach((match: Statement) => {
-          lrs.removeResource(match.subject);
-        });
+          if (s
+              && s !== lrs.namespaces.sp('Variable')
+              && p === lrs.namespaces.sp('Variable')
+              && o === lrs.namespaces.sp('Variable')
+          ) {
+              lrs.api.invalidate(s);
+              lrs.store.removeResource(s);
+          } else {
+              lrs.store.match(
+                  s === lrs.namespaces.sp('Variable') ? null : s,
+                  p === lrs.namespaces.sp('Variable') ? null : p,
+                  o === lrs.namespaces.sp('Variable') ? null : o,
+              ).forEach((match) => {
+                  lrs.api.invalidate(match.subject);
+                  lrs.store.removeResource(match.subject);
+              });
+          }
       });
+}
+
+function processSupplant(delta: Quadruple[], lrs: LinkReduxLRSType) {
+    const supplants = delta.filter(([, , , why]) => why === lrs.namespaces.ll('supplant'));
+
+    supplants.forEach(([s]) => {
+        lrs.store.removeResource(s);
+    });
+    supplants.forEach(([s, p, o]) => {
+        lrs.store.addStatements([new Statement(s, p, o)]);
+    });
 }
 
 function arguDeltaProcessor(lrs: LinkReduxLRSType) {
@@ -51,6 +72,7 @@ function arguDeltaProcessor(lrs: LinkReduxLRSType) {
     },
 
     processDelta(delta: Quadruple[]) {
+      processSupplant(delta, lrs);
       processRemove(delta, lrs);
       processInvalidate(delta, lrs);
 
