0c6d2683ba7f4c75dfd86fbb1af538d084bd9218
Arthur Dingemans <arthur@argu.co>
2019-08-23 16:45:00 +0200

    [AOD][RIVM-2] Use schema:url in ActionWidget when present

--- app/views/Action/ActionWidget.jsx
+++ app/views/Action/ActionWidget.jsx
@@ -4,42 +4,50 @@ import {
   subjectType,
 } from 'link-redux';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 
 import { retrievePath } from '../../helpers/iris';
 import { NS } from '../../helpers/LinkedRenderStore';
 import { widgetTopologyTopology } from '../../topologies/WidgetTopology/WidgetTopology';
 import Button from '../../components/Button';
 
-class ActionWidget extends Component {
-  static type = NS.schema('CreateAction');
-
-  static topology = widgetTopologyTopology;
-
-  static mapDataToProps = [NS.schema('name')];
-
-  static propTypes = {
-    children: PropTypes.element,
-    name: linkType,
-    subject: subjectType,
-  };
-
-  render() {
-    if (this.props.children) {
-      return this.props.children;
-    }
-
-    return (
-      <div>
-        <Button
-          href={retrievePath(this.props.subject.value)}
-          title={this.props.name && this.props.name.toString()}
-        >
-          {this.props.name.value}
-        </Button>
-      </div>
-    );
+const ActionWidget = ({
+  children,
+  name,
+  subject,
+  url,
+}) => {
+  if (children) {
+    return children;
   }
-}
+  const href = url?.value || subject.value;
+
+  return (
+    <div>
+      <Button
+        href={retrievePath(href)}
+        title={name && name.toString()}
+      >
+        {name.value}
+      </Button>
+    </div>
+  );
+};
+
+ActionWidget.type = NS.schema('Action');
+
+ActionWidget.topology = widgetTopologyTopology;
+
+ActionWidget.mapDataToProps = [
+  NS.schema('name'),
+  NS.schema('url'),
+];
+
+ActionWidget.propTypes = {
+  children: PropTypes.element,
+  name: linkType,
+  subject: subjectType,
+  url: linkType,
+};
 
 export default register(ActionWidget);
