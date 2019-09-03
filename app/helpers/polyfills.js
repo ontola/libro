598619d6e569db30dbd36bc8a665a2f7d7e049f4
Arthur Dingemans <arthur@argu.co>
2019-09-03 17:11:23 +0200

    [AOD-632] Add polyfills for react-intl
    https://github.com/formatjs/react-intl/blob/master/docs/Upgrade-Guide.md#migrate-to-using-native-intl-apis

--- app/helpers/polyfills.js
+++ app/helpers/polyfills.js
@@ -1,7 +1,9 @@
 /* eslint-disable global-require */
+import '@formatjs/intl-relativetimeformat/polyfill';
 import 'core-js/features/array/flat';
 import 'core-js/features/array/flat-map';
 import 'core-js/features/array/from';
+import 'intl-pluralrules';
 
 // Removes the rubber banding in iOS
 require('inobounce');
