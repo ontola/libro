600dc55689fdd44aa958fb2ed577f1f2fe093fba
Arthur Dingemans <arthur@argu.co>
2019-09-17 12:17:09 +0200

    [AOD][RIVM-2] Add LabeledAttribute

---
+++ app/components/LabeledAttribute/index.jsx
@@ -0,0 +1,41 @@
+import {
+  LinkedResourceContainer,
+  Property,
+  linkType,
+  useLRS,
+  useLinkRenderContext,
+} from 'link-redux';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import './LabeledAttribute.scss';
+import { Heading } from '../../components';
+
+const LabeledAttribute = ({
+  label,
+  propertyLabel,
+  wrapper,
+}) => {
+  const lrs = useLRS();
+  const { subject } = useLinkRenderContext();
+  const Wrapper = wrapper || 'div';
+
+  if (!lrs.getResourceProperty(subject, label)) {
+    return null;
+  }
+
+  return (
+    <Wrapper className="LabeledAttribute">
+      <Heading size={4}>{propertyLabel || <LinkedResourceContainer subject={label} />}</Heading>
+      <Property label={label} limit={Infinity} />
+    </Wrapper>
+  );
+};
+
+LabeledAttribute.propTypes = {
+  label: linkType,
+  propertyLabel: PropTypes.string,
+  wrapper: PropTypes.string,
+};
+
+export default LabeledAttribute;
