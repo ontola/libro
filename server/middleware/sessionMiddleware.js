70a3116855769822c7717e35290443122cc99b52
Arthur Dingemans <arthur@argu.co>
2019-09-19 16:37:30 +0200

    [AOD-0] Remove monitoring redis

--- server/middleware/sessionMiddleware.js
+++ server/middleware/sessionMiddleware.js
@@ -3,25 +3,11 @@ import session from 'express-session';
 import Redis from 'ioredis';
 
 import {
-  logLevel,
   redisAddress,
   sessionSecret,
 } from '../config';
 
 export const client = new Redis(redisAddress);
-
-if (logLevel === 'debug') {
-  client
-    .monitor()
-    .then((monitor) => {
-      monitor.on('monitor', (time, args, source, database) => {
-        if (database === '0') {
-          // eslint-disable-next-line no-console
-          console.debug(`[REDIS] ${time}: '${database}', '${source}', '${args.join(',')}'`);
-        }
-      });
-    });
-}
 
 const RedisStore = connectRedis(session);
 const redisStore = new RedisStore({
