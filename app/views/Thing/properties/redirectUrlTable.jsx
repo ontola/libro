9422774ef17f8e9c89e1798e210338bf8405b0b5
Arthur Dingemans <arthur@argu.co>
2019-12-04 14:04:18 +0100

    [AOD][ARGU-1808] Render urls in table link

--- app/views/Thing/properties/redirectUrlTable.jsx
+++ app/views/Thing/properties/redirectUrlTable.jsx
@@ -1,26 +1,24 @@
 import schema from '@ontologies/schema';
-import { isDifferentOrigin } from 'link-lib';
 import {
-  LinkedResourceContainer,
   linkedPropType,
   register,
 } from 'link-redux';
 import React from 'react';
 
+import Link from '../../../components/Link';
 import argu from '../../../ontology/argu';
+import ontola from '../../../ontology/ontola';
 import { tableRowTopology } from '../../../topologies/TableRow';
 
-const RedirectUrlTable = ({ linkedProp }) => {
-  if (isDifferentOrigin(linkedProp)) {
-    return linkedProp.value;
-  }
-
-  return <LinkedResourceContainer subject={linkedProp} />;
-};
+const RedirectUrlTable = ({ linkedProp }) => (
+  <Link to={linkedProp.value}>
+    {linkedProp.value}
+  </Link>
+);
 
 RedirectUrlTable.type = schema.Thing;
 
-RedirectUrlTable.property = argu.redirectUrl;
+RedirectUrlTable.property = [argu.redirectUrl, ontola.href];
 
 RedirectUrlTable.topology = tableRowTopology;
 
