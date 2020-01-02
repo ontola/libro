d8e45caff3c15ff74cabdc03cab568fb00c8989f
Arthur Dingemans <arthur@argu.co>
2020-01-02 09:25:26 +0100

    [AOD][ARGU-1982] Add description to meta tags

--- app/components/Metadata/index.jsx
+++ app/components/Metadata/index.jsx
@@ -2,19 +2,26 @@ import rdf from '@ontologies/core';
 import dcterms from '@ontologies/dcterms';
 import rdfs from '@ontologies/rdfs';
 import schema from '@ontologies/schema';
-import { link, lrsType } from 'link-redux';
+import {
+  link,
+  linkType,
+  lrsType,
+  subjectType,
+} from 'link-redux';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import ontola from '../../ontology/ontola';
 import { getMetaContent } from '../../helpers/arguHelpers';
+import { NS } from '../../helpers/LinkedRenderStore';
 
 const Metadata = ({
   coverPhoto,
-  identifier,
   lrs,
   name,
+  subject,
+  text,
 }) => {
   const coverURL = coverPhoto
     && lrs.getResourceProperty(rdf.namedNode(coverPhoto), ontola.imgUrl1500x2000);
@@ -23,19 +30,22 @@ const Metadata = ({
   return (
     <Helmet>
       <title>{name && name.length > 0 ? name : appName}</title>
-      {identifier && <link href={identifier} itemProp="url" rel="canonical" />}
-      {identifier && <meta content={identifier} property="og:url" />}
+      <link href={subject.value} itemProp="url" rel="canonical" />
+      <meta content={subject.value} property="og:url" />
       <meta content={[name, appName].filter(Boolean).join(' | ')} property="og:title" />
       {coverURL && <meta content={coverURL.value} id="og:image" property="og:image" />}
+      {text && <meta content={text} id="og:description" property="og:description" />}
+      {text && <meta content={text} id="description" name="description" property="description" />}
     </Helmet>
   );
 };
 
 Metadata.propTypes = {
   coverPhoto: PropTypes.string,
-  identifier: PropTypes.string.isRequired,
   lrs: lrsType,
   name: PropTypes.string.isRequired,
+  subject: subjectType,
+  text: linkType,
 };
 
 export default link(
@@ -43,6 +53,7 @@ export default link(
     coverPhoto: ontola.coverPhoto,
     identifier: dcterms.identifier,
     name: [schema.name, rdfs.label],
+    text: [NS.dbo('abstract'), schema.description, schema.text],
   },
   { returnType: 'value' }
 )(Metadata);
