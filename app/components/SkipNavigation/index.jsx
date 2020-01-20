86e3b4f274546ef26ef3b3f9dd91167549fce0d8
Joep Meindertsma <joep@argu.co>
2020-01-20 14:16:19 +0100

    [AOD-699] use makeStyles for SkipNavigation

--- app/components/SkipNavigation/index.jsx
+++ app/components/SkipNavigation/index.jsx
@@ -2,11 +2,33 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { makeStyles } from '@material-ui/core/styles';
 
-import './SkipNavigation.scss';
-
 const useStyles = makeStyles((theme) => ({
-  root: {
+  link: {
+    '&:focus': {
+      clip: 'auto',
+      color: theme.palette.primary.contrastText,
+      fontWeight: 'bold',
+      height: 'auto',
+      lineHeight: '3.2rem',
+      padding: '0 10px',
+      textAlign: 'center',
+      textDecoration: 'none',
+      top: 0,
+      width: '100%',
+    },
     backgroundColor: theme.palette.primary.main,
+    clip: 'rect(1px, 1px, 1px, 1px)',
+    height: '1px',
+    margin: '0',
+    overflow: 'hidden',
+    position: 'fixed',
+    top: '-100px',
+    width: '1px',
+    willChange: 'transform',
+  },
+  root: {
+    position: 'fixed',
+    zIndex: 1500,
   },
 }));
 
@@ -14,9 +36,9 @@ const SkipNavigation = () => {
   const classes = useStyles();
 
   return (
-    <div className="SkipNavigation">
+    <div className={`SkipNavigation ${classes.root}`}>
       <a
-        className={`SkipNavigation__link ${classes.root}`}
+        className={`SkipNavigation__link ${classes.link}`}
         href="#start-of-content"
       >
         <FormattedMessage
@@ -25,7 +47,7 @@ const SkipNavigation = () => {
         />
       </a>
       <a
-        className={`SkipNavigation__link ${classes.root}`}
+        className={`SkipNavigation__link ${classes.link}`}
         href="mailto:info@argu.co?subject=Moeite met navigeren"
       >
         <FormattedMessage
