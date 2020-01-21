bca65c2bbabf2d71e824133dfe65f5e8622c1678
Arthur Dingemans <arthur@argu.co>
2020-01-21 13:10:49 +0100

    [AOD][ARGU-1987] Wait with rendering CommentSection until creator is loaded

---
+++ app/views/Comment/CommentContainer.jsx
@@ -0,0 +1,62 @@
+import schema from '@ontologies/schema';
+import { Property, register } from 'link-redux';
+import React from 'react';
+
+import { CardContent } from '../../components';
+import { LoadingOpinion } from '../../components/Loading';
+import app from '../../ontology/app';
+import argu from '../../ontology/argu';
+import ontola from '../../ontology/ontola';
+import ActionsBar from '../../topologies/ActionsBar';
+import Card from '../../topologies/Card';
+import CardAppendix from '../../topologies/Card/CardAppendix';
+import DetailsBar from '../../topologies/DetailsBar';
+import { connectHighlighting, hightlightPropTypes } from '../../containers/Highlight';
+import { containerTopology } from '../../topologies/Container';
+
+const CommentContainer = ({ depth = 0, highlighted }) => (
+  <React.Fragment>
+    <Card shine={highlighted}>
+      <DetailsBar
+        right={(
+          <Property label={ontola.actionsMenu} />
+        )}
+      >
+        <Property label={schema.creator} />
+        <Property label={schema.dateCreated} />
+      </DetailsBar>
+      <CardContent>
+        <Property label={argu.opinion} onError={() => null} onLoad={LoadingOpinion} />
+        <Property label={schema.text} />
+      </CardContent>
+      <ActionsBar small>
+        <Property label={ontola.favoriteAction} onLoad={() => null} />
+        <Property label={schema.comment} onLoad={() => null}>
+          <Property omniform label={ontola.createAction} />
+        </Property>
+      </ActionsBar>
+      <CardAppendix>
+        <Property forceRender clickToOpen={false} label={app.omniform} />
+      </CardAppendix>
+    </Card>
+    <Property
+      clickToOpen
+      depth={depth + 1}
+      label={schema.comment}
+      onLoad={() => null}
+    />
+  </React.Fragment>
+);
+
+CommentContainer.hoc = [connectHighlighting];
+
+CommentContainer.type = [
+  schema.Comment,
+  argu.Comment,
+];
+
+CommentContainer.topology = containerTopology;
+
+CommentContainer.propTypes = hightlightPropTypes;
+
+export default register(CommentContainer);
---
+++ app/views/Comment/CommentSection.jsx
@@ -0,0 +1,61 @@
+import schema from '@ontologies/schema';
+import {
+  Property,
+  Resource,
+  register,
+  subjectType,
+  useDataInvalidation,
+  useLRS,
+  useProperty,
+} from 'link-redux';
+import React from 'react';
+
+import { connectHighlighting, hightlightPropTypes } from '../../containers/Highlight';
+import argu from '../../ontology/argu';
+import CardMicroRow, { cardMicroRowTopology } from '../../topologies/Card/CardMicroRow';
+import { entityIsLoaded } from '../../helpers/data';
+import { cardAppendixTopology } from '../../topologies/Card/CardAppendix';
+import { cardListTopology } from '../../topologies/Card/CardList';
+import { cardRowTopology } from '../../topologies/Card/CardRow';
+
+const CommentSection = ({ highlighted, subject }) => {
+  const lrs = useLRS();
+  const [creator] = useProperty(schema.creator);
+  useDataInvalidation({
+    dataSubjects: [creator],
+    subject,
+  });
+
+  if (creator && !entityIsLoaded(lrs, creator)) {
+    return <Resource subject={creator} />;
+  }
+
+  return (
+    <CardMicroRow highlighted={highlighted}>
+      <Property
+        label={schema.creator}
+      />&#9;<Property label={schema.text} />
+    </CardMicroRow>
+  );
+};
+
+CommentSection.hocs = [connectHighlighting];
+
+CommentSection.type = [
+  schema.Comment,
+  argu.Comment,
+];
+
+CommentSection.topology = [
+  cardAppendixTopology,
+  cardListTopology,
+  cardMicroRowTopology,
+  cardRowTopology,
+];
+
+CommentSection.propTypes = {
+  subject: subjectType,
+  ...hightlightPropTypes,
+};
+
+export default register(CommentSection);
