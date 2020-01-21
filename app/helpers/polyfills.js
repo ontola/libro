37d58b7d89b72e21bd59c0bc2d7e9ecb4712461d
Joep Meindertsma <joep@argu.co>
2020-01-21 12:07:59 +0100

    [AOD-431] Remove iNoBounce fix scrolling ios

--- app/helpers/polyfills.js
+++ app/helpers/polyfills.js
@@ -5,6 +5,4 @@ import 'core-js/features/array/flat-map';
 import 'core-js/features/array/from';
 import 'intl-pluralrules';
 
-// Removes the rubber banding in iOS
-require('inobounce');
 require('smoothscroll-polyfill').polyfill();
