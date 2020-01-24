b8c4ae6cc96f193f89d425ba445345e224f01663
Jurrian <jurrian@argu.co>
2020-01-24 16:10:17 +0100

    core#236 Add useMultipleFetching hook

---
+++ app/hooks/useMultipleFetching.ts
@@ -0,0 +1,25 @@
+import rdfFactory, { isBlankNode } from '@ontologies/core';
+import { normalizeType } from 'link-lib';
+import { DataInvalidationProps, useLRS } from 'link-redux';
+import React from 'react';
+
+export const useMultipleFetching = (
+    props: DataInvalidationProps,
+    lastUpdate?: number,
+    setError?: (e: Error) => void,
+) => {
+    const lrs = useLRS();
+    React.useEffect(() => {
+        for (const s of normalizeType(props.dataSubjects)) {
+            if (s && lrs.shouldLoadResource(s)) {
+                if (isBlankNode(s)) {
+                    if (!setError) {
+                        throw new Error('No setError given');
+                    }
+                    return setError(new TypeError('TODO: blankNodeWarn'));
+                }
+                lrs.queueEntity(s);
+            }
+        }
+    }, [rdfFactory.id(props.subject), lastUpdate]);
+};
