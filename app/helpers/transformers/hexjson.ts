0da2917b2a7536475a838bda97779982e2008e0d
Fletcher91 <thom@argu.co>
2020-03-03 15:36:58 +0100

    core#128 core#216 Implement fallbacks when ReadableStream is not available

--- app/helpers/transformers/hexjson.ts
+++ app/helpers/transformers/hexjson.ts
@@ -1,55 +1,66 @@
 import rdf, { BlankNode, Quad, SomeTerm } from '@ontologies/core';
 // @ts-ignore
 import NdjsonStream from 'can-ndjson-stream';
-import { ResponseAndFallbacks } from 'link-lib';
+import { ExtensionResponse, RDFLibFetcherResponse, ResponseAndFallbacks } from 'link-lib';
 import { LinkReduxLRSType } from 'link-redux';
+
+let hasReadableStreamConstructor = false;
+try {
+  // tslint:disable-next-line:no-empty no-unused-expression
+  new ReadableStream({ start() {} });
+  hasReadableStreamConstructor = true;
+} catch (e) {
+  // ignore
+}
 
 export default {
   acceptValue: 1.0,
   mediaTypes: ['application/hex+x-ndjson'],
 
   transformer: (store: LinkReduxLRSType) => (res: ResponseAndFallbacks): Promise<Quad[]> => {
-    if (res instanceof Response) {
-      const bnMap: { [s: string]: BlankNode } = {};
-      // Skip the (expensive) proxy object when parsing
-      const quad = rdf.quad.bind(rdf);
-      const literal = rdf.literal.bind(rdf);
-      const namedNode = rdf.namedNode.bind(rdf);
-      const blankNodeF = rdf.blankNode.bind(rdf);
-      const defaultGraph = rdf.defaultGraph.bind(rdf);
+    const bnMap: { [s: string]: BlankNode } = {};
+    // Skip the (expensive) proxy object when parsing
+    const quad = rdf.quad.bind(rdf);
+    const literal = rdf.literal.bind(rdf);
+    const namedNode = rdf.namedNode.bind(rdf);
+    const blankNodeF = rdf.blankNode.bind(rdf);
+    const defaultGraph = rdf.defaultGraph.bind(rdf);
 
-      const blankNode = (v: string) => {
-        if (!bnMap[v]) {
-          bnMap[v] = blankNodeF();
-        }
-        return bnMap[v];
-      };
+    const blankNode = (v: string) => {
+      if (!bnMap[v]) {
+        bnMap[v] = blankNodeF();
+      }
+      return bnMap[v];
+    };
 
-      const object = (v: string, dt: string, l: string): SomeTerm => {
-        if (l) {
-          return literal(v, l);
-        } else if (dt === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#namedNode') {
-          return namedNode(v);
-        } else if (dt === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#blankNode') {
-          return blankNode(v);
-        }
+    const object = (v: string, dt: string, l: string): SomeTerm => {
+      if (l) {
+        return literal(v, l);
+      } else if (dt === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#namedNode') {
+        return namedNode(v);
+      } else if (dt === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#blankNode') {
+        return blankNode(v);
+      }
 
-        return literal(v, namedNode(dt));
-      };
+      return literal(v, namedNode(dt));
+    };
 
-      const lineToQuad = (h: string[]) => quad(
-        h[0].startsWith('_:') ? blankNode(h[0]) : namedNode(h[0]),
-        namedNode(h[1]),
-        object(h[2], h[3], h[4]),
-        h[5] ? namedNode(h[5]) : defaultGraph(),
-      );
+    const lineToQuad = (h: string[]) => quad(
+      h[0].startsWith('_:') ? blankNode(h[0]) : namedNode(h[0]),
+      namedNode(h[1]),
+      object(h[2], h[3], h[4]),
+      h[5] ? namedNode(h[5]) : defaultGraph(),
+    );
 
+    const delta: Quad[] = [];
+    let parse;
+
+    if (res instanceof Response && hasReadableStreamConstructor) {
       const stream = new NdjsonStream(res.body);
       const reader = stream.getReader();
-      const delta: Quad[] = [];
 
       let read: any;
-      return reader
+      parse = reader
         .read()
         .then(read = (result: { done: boolean, value: string[] }) => {
           if (result.done) {
@@ -58,11 +69,31 @@ export default {
 
           delta.push(lineToQuad(result.value));
           return reader.read().then(read);
-        })
-        .then(() => store.queueDelta(delta, res.hasOwnProperty('expedite') ? (res as any).expedite : false))
-        .then(() => []);
+        });
     } else {
-      return Promise.resolve([]);
+      let body;
+      if (res instanceof Response) {
+        body = res.text();
+      } else if (typeof XMLHttpRequest !== 'undefined'
+        && res instanceof XMLHttpRequest
+        || typeof (res as any).responseText === 'string') {
+        body = Promise.resolve((res as XMLHttpRequest | RDFLibFetcherResponse).responseText);
+      } else {
+        body = Promise.resolve((res as ExtensionResponse).body);
+      }
+
+      parse = body
+        .then((text) => {
+          for (const line of text.split('\n')) {
+            if (line.length > 0) {
+              delta.push(lineToQuad(JSON.parse(line)));
+            }
+          }
+        });
     }
+
+    return parse
+      .then(() => store.queueDelta(delta, res.hasOwnProperty('expedite') ? (res as any).expedite : false))
+      .then(() => []);
   },
 };
