91e0ebc96bb715d8812d80388a944c66837bb824
Arthur Dingemans <arthur@argu.co>
2020-03-19 12:15:27 +0100

    core#247 Add links to Customers

--- app/views/ArguHome/Customer.jsx
+++ app/views/ArguHome/Customer.jsx
@@ -5,20 +5,34 @@ import {
 } from 'link-redux';
 import React from 'react';
 
+import './Customer.scss';
+import Link from '../../components/Link';
+import GridItem from '../../components/Grid/GridItem';
 import argu from '../../ontology/argu';
 import { allTopologies } from '../../topologies';
-import './Customer.scss';
-import GridItem from '../../components/Grid/GridItem';
 
-const Customer = ({ name, image }) => (
-  <GridItem>
-    <div
-      className="Customer"
-      style={{ backgroundImage: `url("${image.value}")` }}
-      title={name.value}
-    />
-  </GridItem>
-);
+const Customer = ({
+  url,
+  name,
+  image,
+}) => {
+  const Wrapper = url ? Link : React.Fragment;
+
+  return (
+    <GridItem>
+      <Wrapper
+        style={{ width: '100%' }}
+        to={url?.value}
+      >
+        <div
+          className="Customer"
+          style={{ backgroundImage: `url("${image.value}")` }}
+          title={name.value}
+        />
+      </Wrapper>
+    </GridItem>
+  );
+};
 
 Customer.type = argu.Customer;
 
@@ -27,11 +41,13 @@ Customer.topology = allTopologies;
 Customer.mapDataToProps = {
   image: schema.image,
   name: schema.name,
+  url: schema.url,
 };
 
 Customer.propTypes = {
   image: linkType,
   name: linkType,
+  url: linkType,
 };
 
 export default register(Customer);
