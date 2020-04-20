4a693eed30b022186c80f035c909f90daa10ad59
Arthur Dingemans <arthur@argu.co>
2020-04-20 09:51:13 +0200

    core#270 Add renderer for boolean type

---
+++ app/views/DataType/default/boolean.jsx
@@ -0,0 +1,35 @@
+import rdfs from '@ontologies/rdfs';
+import xsd from '@ontologies/xsd';
+import { linkedPropType, register } from 'link-redux';
+import { defineMessages, useIntl } from 'react-intl';
+
+import { allTopologies } from '../../../topologies';
+
+const booleanTranslation = defineMessages({
+  false: {
+    defaultMessage: 'No',
+    id: 'https://app.argu.co/i18n/xsd:boolean/false',
+  },
+  true: {
+    defaultMessage: 'Yes',
+    id: 'https://app.argu.co/i18n/xsd:boolean/true',
+  },
+});
+
+const BooleanRenderer = ({ linkedProp }) => {
+  const intl = useIntl();
+
+  return intl.formatMessage(booleanTranslation[linkedProp.value]);
+};
+
+BooleanRenderer.type = rdfs.Literal;
+
+BooleanRenderer.topology = allTopologies;
+
+BooleanRenderer.property = xsd.boolean;
+
+BooleanRenderer.propTypes = {
+  linkedProp: linkedPropType,
+};
+
+export default register(BooleanRenderer);
