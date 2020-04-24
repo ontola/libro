59660cd546303f019858f8cd558e59d3e73ae402
Fletcher91 <thom@argu.co>
2020-04-24 16:21:14 +0200

    core#5 Actually run tests during CI

--- app/__tests__/Search.spec.jsx
+++ app/__tests__/Search.spec.jsx
@@ -16,32 +16,51 @@ describe('Search', () => {
   afterAll(cleanup);
 
   const testIRI = example.ns('test/search?q=keyword');
+  const firstPage = example.ns('test/search?q=keyword&page=1');
   const seqIRI = example.ns('test/search/results');
+  const baseCollection = example.ns('test/search');
 
   const resources = {
     '@id': testIRI.value,
     [rdfx.type]: ontola.SearchResult,
+    [ontola.baseCollection]: baseCollection,
+    [ontola.collectionDisplay]: ontola['collectionDisplay/grid'],
+    [ontola.collectionType]: ontola['collectionType/paginated'],
+    [ontola.iriTemplate]: example.ns('test/search{?q,filter%5B%5D*,sort%5B%5D*,page,page_size}{#fragment}').value,
     [dcterms.identifier]: testIRI,
     [argu.query]: 'keyword',
-    [argu.took]: 240,
     [as.totalItems]: 3,
-    [as.items]: seq([
-      {
-        '@id': example.ns('1'),
-        [rdfx.type]: schema.CreativeWork,
-        [schema.name]: 'Item 1',
-      },
-      {
-        '@id': example.ns('2'),
-        [rdfx.type]: argu.Question,
-        [schema.name]: 'Item 2',
-      },
-      {
-        '@id': example.ns('3'),
-        [rdfx.type]: schema.Thing,
-        [schema.name]: 'Item 3',
-      },
-    ], seqIRI),
+    [as.name]: '3 results found',
+    [as.first]: firstPage,
+    [as.last]: firstPage,
+    [ontola.pages]: {
+      '@id': firstPage,
+      [rdfx.type]: [
+        ontola.PaginatedView,
+        as.CollectionPage,
+      ],
+      [ontola.collectionDisplay]: ontola['collectionDisplay/grid'],
+      [ontola.baseCollection]: baseCollection,
+      [as.partOf]: testIRI.value,
+      [as.totalItems]: 3,
+      [as.items]: seq([
+        {
+          '@id': example.ns('1'),
+          [rdfx.type]: schema.CreativeWork,
+          [schema.name]: 'Item 1',
+        },
+        {
+          '@id': example.ns('2'),
+          [rdfx.type]: argu.Question,
+          [schema.name]: 'Item 2',
+        },
+        {
+          '@id': example.ns('3'),
+          [rdfx.type]: schema.Thing,
+          [schema.name]: 'Item 3',
+        },
+      ], seqIRI),
+    },
   };
 
   describe('within Page', () => {
@@ -66,7 +85,7 @@ describe('Search', () => {
       expect(getByText('Search')).toBeVisible();
 
       // renders the search info
-      expect(getByText('3 results in 240ms')).toBeVisible();
+      expect(getByText('3 results found')).toBeVisible();
 
       // renders the search results
       expect(getByText('Item 1')).toBeVisible();
