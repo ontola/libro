9f38aaf4bc9e63564d2a0a426a67eb3b2305794c
Arthur Dingemans <arthur@argu.co>
2020-10-30 12:40:08 +0100

    TGL-6384 Extract useMap and useOverlay

---
+++ app/async/MapView/useOverlay.js
@@ -0,0 +1,49 @@
+import { Overlay } from 'ol';
+import OverlayPositioning from 'ol/OverlayPositioning';
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+} from 'react';
+
+const useOverlay = ({
+  map,
+  navigate,
+  overlayPosition,
+  overlayResource,
+}) => {
+  const overlayRef = useRef(document.createElement('div'));
+  const overlay = useMemo(() => {
+    if (map) {
+      const o = new Overlay({
+        autoPan: true,
+        element: overlayRef.current,
+        positioning: OverlayPositioning.TOP_CENTER,
+        stopEvent: true,
+      });
+      map.addOverlay(o);
+
+      return o;
+    }
+
+    return null;
+  }, [map]);
+
+  useEffect(() => {
+    if (overlay && overlayPosition) {
+      overlay.setPosition(overlayPosition);
+    }
+  }, [overlay, overlayPosition]);
+
+  const handleOverlayClick = useCallback((e) => (
+    e.target.className !== 'click-ignore' && navigate && navigate(overlayResource)
+  ), [overlayResource]);
+
+  return {
+    handleOverlayClick,
+    overlayRef,
+  };
+};
+
+export default useOverlay;
