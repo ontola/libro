f479e43e5cde10f1289caab434abb8219fb2cea6
Arthur Dingemans <arthur@argu.co>
2020-10-30 12:40:08 +0100

    TGL-6384 Extract useMap and useOverlay

--- app/async/MapView/MapCanvas.jsx
+++ app/async/MapView/MapCanvas.jsx
@@ -3,22 +3,6 @@ import {
   Resource,
   linkType,
 } from 'link-redux';
-import {
-  Map as OLMap,
-  Overlay,
-  View,
-} from 'ol';
-import { FullScreen, defaults as defaultControls } from 'ol/control';
-import { click, pointerMove } from 'ol/events/condition';
-import { boundingExtent, getCenter } from 'ol/extent';
-import Select from 'ol/interaction/Select';
-import TileLayer from 'ol/layer/Tile';
-import VectorLayer from 'ol/layer/Vector';
-import OverlayPositioning from 'ol/OverlayPositioning';
-import { fromLonLat, toLonLat } from 'ol/proj';
-import Cluster from 'ol/source/Cluster';
-import VectorSource from 'ol/source/Vector';
-import XYZ from 'ol/source/XYZ';
 import PropTypes from 'prop-types';
 import React from 'react';
 import FontAwesome from 'react-fontawesome';
@@ -27,295 +11,41 @@ import { connect } from 'react-redux';
 
 import OverlayContainer from '../../components/OverlayContainer';
 import withReducer from '../../containers/withReducer';
-import { getMetaContent } from '../../helpers/arguHelpers';
-import { handle } from '../../helpers/logging';
 import { popupTopology } from '../../topologies/Popup';
 
-import 'ol/ol.css';
-import useMapStyles from './useMapStyles';
 import { getMapAccessToken } from './actions';
+import 'ol/ol.css';
 import reducer, { MapReducerKey } from './reducer';
 import { getAccessToken, getAccessTokenError } from './selectors';
-
-const DEFAULT_LAT = 52.1344;
-const DEFAULT_LON = 5.1917;
-const DEFAULT_ZOOM = 6.8;
-const TILE_SIZE = 512;
-const CLUSTER_DISTANCE = 40;
-const CLUSTER_ZOOM = 2;
-
-const getStyle = (styleName) => (feature) => {
-  const features = feature?.get('features');
-
-  return (features?.[0] || feature).get(styleName)(feature);
-};
-
-const createMap = ({
-  accessToken,
-  initialLat,
-  initialLon,
-  initialZoom,
-  layerSources,
-  mapRef,
-  overlayRef,
-  tileSource,
-}) => {
-  const { current } = mapRef;
-
-  if (!current || !accessToken || !layerSources || !tileSource) {
-    return [];
-  }
-
-  const layers = [
-    new TileLayer({
-      source: tileSource,
-    }),
-    ...layerSources.map((source) => (
-      new VectorLayer({
-        source,
-        style: getStyle('style'),
-      })
-    )),
-  ];
-
-  const overlay = new Overlay({
-    autoPan: true,
-    element: overlayRef.current,
-    positioning: OverlayPositioning.TOP_CENTER,
-    stopEvent: true,
-  });
-
-  const map = new OLMap({
-    controls: defaultControls({
-      rotate: false,
-    }).extend([
-      new FullScreen(),
-    ]),
-    layers,
-    overlays: [overlay],
-    target: current,
-    view: new View({
-      center: fromLonLat([initialLon || DEFAULT_LON, initialLat || DEFAULT_LAT]),
-      zoom: initialZoom || DEFAULT_ZOOM,
-    }),
-  });
-
-  return [map, overlay];
-};
-
-const updateFeatures = (layerSources, layers) => {
-  if (layerSources) {
-    layerSources.forEach((source, index) => {
-      source.clear(true);
-      const layer = layers[index];
-      if (layer.clustered) {
-        source.source.addFeatures(layer.features.slice() || []);
-      } else {
-        source.addFeatures(layer.features.slice() || []);
-      }
-    });
-  }
-};
-
-const useMap = (props) => {
-  const {
-    accessToken,
-    initialZoom,
-    layers,
-    requestAccessToken,
-    navigate,
-    onMapClick,
-    onSelect,
-    onZoom,
-    overlayResource,
-  } = props;
-  const mapboxTileURL = React.useMemo(() => getMetaContent('mapboxTileURL'));
-  const mapRef = React.useRef();
-  const overlayRef = React.useRef(document.createElement('div'));
-  React.useEffect(() => {
-    if (!accessToken) {
-      requestAccessToken();
-    }
-  }, [accessToken]);
-
-  const [zoom, setZoom] = React.useState(initialZoom || DEFAULT_ZOOM);
-  const [layerSources, setLayerSources] = React.useState(null);
-  const [tileSource, setTileSource] = React.useState(null);
-  const [error, setError] = React.useState(undefined);
-  const [memoizedOverlay, setOverlay] = React.useState(undefined);
-  const [memoizedMap, setMap] = React.useState(undefined);
-
-  const handleError = React.useCallback((e) => {
-    handle(e);
-    setError(true);
-  }, []);
-  const handleMapClick = React.useCallback(
-    onMapClick ? (e) => onMapClick(...toLonLat(e.coordinate), zoom) : undefined,
-    []
-  );
-  const handleLoad = React.useCallback(() => {
-    setError(undefined);
-  }, []);
-  const handleSelect = React.useCallback((e) => {
-    const [feature] = e.selected;
-    const features = feature?.get('features');
-    const selected = features?.[0] || feature;
-
-    if (features?.length > 1) {
-      const bounds = boundingExtent(features.map((f) => f.getGeometry().getCoordinates()));
-      const newZoom = e.mapBrowserEvent.map.getView().getZoom() + CLUSTER_ZOOM;
-
-      e.mapBrowserEvent.map.getView().animate({
-        center: getCenter(bounds),
-        zoom: newZoom,
-      });
-    } else if (selected) {
-      if (onSelect) {
-        onSelect(selected);
-      }
-
-      const geometry = selected.getGeometry();
-      if (geometry.getType === 'Point') {
-        memoizedOverlay.setPosition(geometry.getCoordinates());
-      } else {
-        memoizedOverlay.setPosition(getCenter(geometry.getExtent()));
-      }
-    } else if (onSelect) {
-      onSelect(null);
-    }
-  }, [memoizedOverlay]);
-  const handleZoom = React.useCallback(
-    (e) => {
-      const newZoom = e.map.getView().getZoom();
-      if (newZoom !== zoom) {
-        if (onZoom) {
-          onZoom(newZoom);
-        }
-        setZoom(newZoom);
-      }
-    }
-  );
-
-  React.useEffect(() => {
-    if (accessToken) {
-      const sources = layers.map((layer) => {
-        const vectorSource = new VectorSource();
-        let source;
-        if (layer.clustered) {
-          source = new Cluster({
-            distance: CLUSTER_DISTANCE,
-            source: vectorSource,
-          });
-        } else {
-          source = vectorSource;
-        }
-
-        return source;
-      });
-
-      setLayerSources(sources);
-      const source = new XYZ({
-        tileSize: [TILE_SIZE, TILE_SIZE],
-        url: `${mapboxTileURL}/tiles/{z}/{x}/{y}?access_token=${accessToken}`,
-      });
-      source.addEventListener('tileloadend', handleLoad);
-      source.addEventListener('tileloaderr', handleError);
-      setTileSource(source);
-
-      return () => {
-        source.removeEventListener('tileloadend', handleLoad);
-        source.removeEventListener('tileloaderr', handleError);
-      };
-    }
-
-    return () => {};
-  }, [accessToken]);
-
-  React.useEffect(() => {
-    const [map, overlay] = createMap({
-      ...props,
-      layerSources,
-      mapRef,
-      overlayRef,
-      tileSource,
-    });
-    setOverlay(overlay);
-    setMap(map);
-
-    if (map) {
-      if (handleMapClick) {
-        map.addEventListener('click', handleMapClick);
-      }
-      if (handleZoom) {
-        map.addEventListener('moveend', handleZoom);
-      }
-    }
-
-    return () => {
-      if (map) {
-        if (handleMapClick) {
-          map.removeEventListener('click', handleMapClick);
-        }
-        if (handleZoom) {
-          map.removeEventListener('moveend', handleZoom);
-        }
-        map.setTarget(null);
-      }
-    };
-  }, [mapRef.current, accessToken, layerSources, tileSource]);
-
-  React.useEffect(() => {
-    updateFeatures(layerSources, layers);
-  }, [!!layerSources, ...layers]);
-
-  React.useEffect(() => {
-    if (memoizedMap) {
-      const select = new Select({
-        condition: click,
-        style: false,
-      });
-      select.on('select', handleSelect);
-      memoizedMap.addInteraction(select);
-
-      const hover = new Select({
-        condition: pointerMove,
-        style: getStyle('hoverStyle'),
-      });
-      memoizedMap.addInteraction(hover);
-
-      return () => {
-        memoizedMap.removeInteraction(select);
-        memoizedMap.removeInteraction(hover);
-      };
-    }
-
-    return () => {};
-  }, [handleSelect, memoizedMap]);
-
-  const handleOverlayClick = React.useCallback((e) => (
-    e.target.className !== 'click-ignore' && navigate && navigate(overlayResource)
-  ), [overlayResource]);
-
-  return {
-    error,
-    handleOverlayClick,
-    mapRef,
-    overlayRef,
-  };
-};
+import useMap from './useMap';
+import useMapStyles from './useMapStyles';
+import useOverlay from './useOverlay';
 
 const MapCanvas = (props) => {
   const {
     accessTokenError,
     large,
+    navigate,
     overlayResource,
   } = props;
+  const [overlayPosition, setOverlayPosition] = React.useState();
   const {
     error,
+    map,
     mapRef,
+  } = useMap({
+    setOverlayPosition,
+    ...props,
+  });
+  const {
     handleOverlayClick,
     overlayRef,
-  } = useMap(props);
+  } = useOverlay({
+    map,
+    navigate,
+    overlayPosition,
+    overlayResource,
+  });
   const classes = useMapStyles();
   const wrapperClassName = classNames({
     [classes.container]: true,
@@ -367,6 +97,7 @@ const MapCanvas = (props) => {
 MapCanvas.propTypes = {
   accessTokenError: PropTypes.string,
   large: PropTypes.bool,
+  navigate: PropTypes.func,
   overlayResource: linkType,
 };
 
---
+++ app/async/MapView/useMap.js
@@ -0,0 +1,276 @@
+import {
+  Map as OLMap,
+  View,
+} from 'ol';
+import {
+  FullScreen,
+  defaults as defaultControls,
+} from 'ol/control';
+import { click, pointerMove } from 'ol/events/condition';
+import { boundingExtent, getCenter } from 'ol/extent';
+import Select from 'ol/interaction/Select';
+import TileLayer from 'ol/layer/Tile';
+import VectorLayer from 'ol/layer/Vector';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import Cluster from 'ol/source/Cluster';
+import VectorSource from 'ol/source/Vector';
+import XYZ from 'ol/source/XYZ';
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+
+import { getMetaContent } from '../../helpers/arguHelpers';
+import { handle } from '../../helpers/logging';
+
+const DEFAULT_LAT = 52.1344;
+const DEFAULT_LON = 5.1917;
+const DEFAULT_ZOOM = 6.8;
+const TILE_SIZE = 512;
+const CLUSTER_DISTANCE = 40;
+const CLUSTER_ZOOM = 2;
+
+const getStyle = (styleName) => (feature) => {
+  const features = feature?.get('features');
+
+  return (features?.[0] || feature).get(styleName)(feature);
+};
+
+const updateFeatures = (layerSources, layers) => {
+  if (layerSources) {
+    layerSources.forEach((source, index) => {
+      source.clear(true);
+      const layer = layers[index];
+      if (layer.clustered) {
+        source.source.addFeatures(layer.features.slice() || []);
+      } else {
+        source.addFeatures(layer.features.slice() || []);
+      }
+    });
+  }
+};
+
+const createMap = ({
+  accessToken,
+  initialLat,
+  initialLon,
+  initialZoom,
+  layerSources,
+  mapRef,
+  tileSource,
+}) => {
+  const { current } = mapRef;
+
+  if (!current || !accessToken || !layerSources || !tileSource) {
+    return null;
+  }
+
+  const layers = [
+    new TileLayer({
+      source: tileSource,
+    }),
+    ...layerSources.map((source) => (
+      new VectorLayer({
+        source,
+        style: getStyle('style'),
+      })
+    )),
+  ];
+
+  const map = new OLMap({
+    controls: defaultControls({
+      rotate: false,
+    }).extend([
+      new FullScreen(),
+    ]),
+    layers,
+    target: current,
+    view: new View({
+      center: fromLonLat([initialLon || DEFAULT_LON, initialLat || DEFAULT_LAT]),
+      zoom: initialZoom || DEFAULT_ZOOM,
+    }),
+  });
+
+  return map;
+};
+
+const useMap = (props) => {
+  const {
+    accessToken,
+    initialZoom,
+    layers,
+    requestAccessToken,
+    setOverlayPosition,
+    onMapClick,
+    onSelect,
+    onZoom,
+  } = props;
+  const mapboxTileURL = useMemo(() => getMetaContent('mapboxTileURL'));
+  const mapRef = useRef();
+  useEffect(() => {
+    if (!accessToken) {
+      requestAccessToken();
+    }
+  }, [accessToken]);
+
+  const [zoom, setZoom] = useState(initialZoom || DEFAULT_ZOOM);
+  const [layerSources, setLayerSources] = useState(null);
+  const [tileSource, setTileSource] = useState(null);
+  const [error, setError] = useState(undefined);
+  const [memoizedMap, setMap] = useState(undefined);
+
+  const handleError = useCallback((e) => {
+    handle(e);
+    setError(true);
+  }, []);
+  const handleMapClick = useCallback(
+    onMapClick ? (e) => onMapClick(...toLonLat(e.coordinate), zoom) : undefined,
+    []
+  );
+  const handleLoad = useCallback(() => {
+    setError(undefined);
+  }, []);
+  const handleSelect = useCallback((e) => {
+    const [feature] = e.selected;
+    const features = feature?.get('features');
+    const selected = features?.[0] || feature;
+
+    if (features?.length > 1) {
+      const bounds = boundingExtent(features.map((f) => f.getGeometry().getCoordinates()));
+      const newZoom = e.mapBrowserEvent.map.getView().getZoom() + CLUSTER_ZOOM;
+
+      e.mapBrowserEvent.map.getView().animate({
+        center: getCenter(bounds),
+        zoom: newZoom,
+      });
+    } else if (selected) {
+      if (onSelect) {
+        onSelect(selected);
+      }
+
+      const geometry = selected.getGeometry();
+      if (geometry.getType === 'Point') {
+        setOverlayPosition(geometry.getCoordinates());
+      } else {
+        setOverlayPosition(getCenter(geometry.getExtent()));
+      }
+    } else if (onSelect) {
+      onSelect(null);
+    }
+  }, []);
+  const handleZoom = useCallback(
+    (e) => {
+      const newZoom = e.map.getView().getZoom();
+      if (newZoom !== zoom) {
+        if (onZoom) {
+          onZoom(newZoom);
+        }
+        setZoom(newZoom);
+      }
+    }
+  );
+
+  useEffect(() => {
+    if (accessToken) {
+      const sources = layers.map((layer) => {
+        const vectorSource = new VectorSource();
+        let source;
+        if (layer.clustered) {
+          source = new Cluster({
+            distance: CLUSTER_DISTANCE,
+            source: vectorSource,
+          });
+        } else {
+          source = vectorSource;
+        }
+
+        return source;
+      });
+
+      setLayerSources(sources);
+      const source = new XYZ({
+        tileSize: [TILE_SIZE, TILE_SIZE],
+        url: `${mapboxTileURL}/tiles/{z}/{x}/{y}?access_token=${accessToken}`,
+      });
+      source.addEventListener('tileloadend', handleLoad);
+      source.addEventListener('tileloaderr', handleError);
+      setTileSource(source);
+
+      return () => {
+        source.removeEventListener('tileloadend', handleLoad);
+        source.removeEventListener('tileloaderr', handleError);
+      };
+    }
+
+    return () => {};
+  }, [accessToken]);
+
+  useEffect(() => {
+    const map = createMap({
+      ...props,
+      layerSources,
+      mapRef,
+      tileSource,
+    });
+    setMap(map);
+
+    if (map) {
+      if (handleMapClick) {
+        map.addEventListener('click', handleMapClick);
+      }
+      if (handleZoom) {
+        map.addEventListener('moveend', handleZoom);
+      }
+    }
+
+    return () => {
+      if (map) {
+        if (handleMapClick) {
+          map.removeEventListener('click', handleMapClick);
+        }
+        if (handleZoom) {
+          map.removeEventListener('moveend', handleZoom);
+        }
+        map.setTarget(null);
+      }
+    };
+  }, [mapRef.current, accessToken, layerSources, tileSource]);
+
+  useEffect(() => {
+    updateFeatures(layerSources, layers);
+  }, [!!layerSources, ...layers]);
+
+  useEffect(() => {
+    if (memoizedMap) {
+      const select = new Select({
+        condition: click,
+        style: false,
+      });
+      select.on('select', handleSelect);
+      memoizedMap.addInteraction(select);
+
+      const hover = new Select({
+        condition: pointerMove,
+        style: getStyle('hoverStyle'),
+      });
+      memoizedMap.addInteraction(hover);
+
+      return () => {
+        memoizedMap.removeInteraction(select);
+        memoizedMap.removeInteraction(hover);
+      };
+    }
+
+    return () => {};
+  }, [handleSelect, memoizedMap]);
+
+  return {
+    error,
+    mapRef,
+  };
+};
+
+export default useMap;
