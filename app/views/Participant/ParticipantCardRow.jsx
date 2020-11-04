e7adbcb77be6c828258d68ab79b60d5fd1cdc07e
Arthur Dingemans <arthur@argu.co>
2020-11-04 15:26:07 +0100

    TGL-7350 Show potentialActions for participants in cardRow

--- app/views/Participant/ParticipantCardRow.jsx
+++ app/views/Participant/ParticipantCardRow.jsx
@@ -9,21 +9,27 @@ import CardContent from '../../components/Card/CardContent';
 import HeaderWithMenu from '../../components/HeaderWithMenu';
 import ontola from '../../ontology/ontola';
 import teamGL from '../../ontology/teamGL';
+import ActionsBar from '../../topologies/ActionsBar';
 import { cardRowTopology } from '../../topologies/Card/CardRow';
 import ContentDetails from '../../topologies/ContentDetails';
 
 const ParticipantCardRow = () => (
-  <CardContent>
-    <HeaderWithMenu
-      menu={<Property label={ontola.actionsMenu} />}
-    >
-      <Property label={schema.name} />
-    </HeaderWithMenu>
-    <ContentDetails>
-      <Property label={teamGL.engagement} />
-      <Property label={teamGL.signedUp} />
-    </ContentDetails>
-  </CardContent>
+  <React.Fragment>
+    <CardContent>
+      <HeaderWithMenu
+        menu={<Property label={ontola.actionsMenu} />}
+      >
+        <Property label={schema.name} />
+      </HeaderWithMenu>
+      <ContentDetails>
+        <Property label={teamGL.engagement} />
+        <Property label={teamGL.signedUp} />
+      </ContentDetails>
+    </CardContent>
+    <ActionsBar>
+      <Property label={ontola.favoriteAction} />
+    </ActionsBar>
+  </React.Fragment>
 );
 
 ParticipantCardRow.type = teamGL.Participant;
