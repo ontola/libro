68cc37490efcdcf449c85b8846857b1f3825306e
Onno Bootsma <onno@argu.co>
2020-11-19 13:58:47 +0100

    core#431 Render empty table cells

--- app/components/TableCells/index.tsx
+++ app/components/TableCells/index.tsx
@@ -1,21 +1,32 @@
 import { normalizeType } from 'link-lib';
-import { Property } from 'link-redux';
+import { Property, useLinkRenderContext, useLRS } from 'link-redux';
 import React from 'react';
 
 import Spinner from '../Spinner';
 
 const TableCells = ({ columns }: { columns: any }) => {
+  const { subject } = useLinkRenderContext();
+  const lrs = useLRS();
+
   if (!Array.isArray(columns)) {
     return <Spinner loading />;
   }
 
-  return columns.map((column) => (
-    <Property
-      forceRender
-      key={normalizeType(column)[0].value}
-      label={column}
-    />
-  ));
+  return columns.map((column) => {
+    if (!lrs.getResourceProperty(subject, column)) {
+      return (
+        <td key={normalizeType(column)[0].value} />
+      );
+    }
+
+    return (
+      <Property
+        forceRender
+        key={normalizeType(column)[0].value}
+        label={column}
+      />
+    );
+  });
 };
 
 export default TableCells;
