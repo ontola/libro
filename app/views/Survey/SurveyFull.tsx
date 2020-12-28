96ca067f373b3325bd39d87c72ea8d71f3c9d795
Arthur Dingemans <arthur@argu.co>
2020-12-28 17:22:55 +0100

    core#457 Hide survey button when expired

---
+++ app/views/Survey/SurveyFull.tsx
@@ -0,0 +1,82 @@
+import { Literal } from '@ontologies/core';
+import rdfx from '@ontologies/rdf';
+import rdfs from '@ontologies/rdfs';
+import schema from '@ontologies/schema';
+import {
+  FC,
+  Property,
+  register,
+} from 'link-redux';
+import React from 'react';
+
+import CardContent from '../../components/Card/CardContent';
+import LinkedDetailDate from '../../components/LinkedDetailDate';
+import isPastDate from '../../helpers/date';
+import argu from '../../ontology/argu';
+import dbo from '../../ontology/dbo';
+import ontola from '../../ontology/ontola';
+import ActionsBar from '../../topologies/ActionsBar';
+import CardMain from '../../topologies/Card/CardMain';
+import Container from '../../topologies/Container';
+import DetailsBar from '../../topologies/DetailsBar';
+import { fullResourceTopology } from '../../topologies/FullResource';
+
+interface PropTypes {
+  expiresAt: Literal;
+  renderPartOf: boolean;
+}
+
+const SurveyFull: FC<PropTypes> = ({
+  expiresAt,
+  renderPartOf,
+}) => {
+  const expired = expiresAt && isPastDate(expiresAt);
+
+  return (
+    <React.Fragment>
+      <Container>
+        {renderPartOf && <Property label={schema.isPartOf} />}
+        <Property label={argu.trashedAt} />
+        <Property label={ontola.publishAction} onLoad={() => null} />
+        <CardMain data-test="Thing-thing">
+          <DetailsBar
+            right={(
+              <React.Fragment>
+                <Property label={ontola.followMenu} />
+                <Property label={ontola.shareMenu} />
+                <Property label={ontola.actionsMenu} />
+              </React.Fragment>
+            )}
+          >
+            <Property label={schema.creator} />
+            <Property label={rdfx.type} />
+            <LinkedDetailDate />
+            <Property label={argu.pinnedAt} />
+            <Property label={argu.expiresAt} />
+            <Property label={argu.followsCount} />
+            <Property label={argu.motionsCount} />
+            <Property label={schema.location} />
+            <Property label={argu.grantedGroups} />
+          </DetailsBar>
+          <CardContent endSpacing noSpacing>
+            <Property label={[schema.name, rdfs.label]} />
+            <Property label={[schema.text, schema.description, dbo.abstract]} />
+            <ActionsBar>
+              {!expired && <Property label={argu.externalIRI} />}
+            </ActionsBar>
+          </CardContent>
+        </CardMain>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+SurveyFull.type = [argu.Survey];
+
+SurveyFull.topology = fullResourceTopology;
+
+SurveyFull.mapDataToProps = {
+  expiresAt: argu.expiresAt,
+};
+
+export default register(SurveyFull);
