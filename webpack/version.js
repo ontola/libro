547730f2d10a131f116f5f2e8dc103678733d726
Arthur Dingemans <arthur@argu.co>
2021-02-09 11:31:19 +0100

    core#12 Throw on missing version

--- webpack/version.js
+++ webpack/version.js
@@ -1,5 +1,15 @@
 const { spawnSync } = require('child_process');
 
+const versionFromTag = () => {
+  const version = spawnSync('git', ['describe', '--tags']).stdout.toString('utf8').trim();
+
+  if (version.length === 0) {
+    throw new Error('Version is missing');
+  }
+
+  return version;
+};
+
 module.exports = typeof __VERSION__ !== 'undefined'
   ? __VERSION__
-  : spawnSync('git', ['describe', '--tags']).stdout.toString('utf8').trim();
+  : versionFromTag();
