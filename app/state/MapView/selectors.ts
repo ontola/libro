7a915cb70932a8c9ac503a2018c3c47eaee4d59a
Arthur Dingemans <arthur@argu.co>
2021-03-26 13:22:56 +0100

    core#2 Fix MapView accessToken selector

--- app/state/MapView/selectors.ts
+++ app/state/MapView/selectors.ts
@@ -1,4 +1,4 @@
 import { MapReducerKey, MapViewStateTree } from './reducer';
 
-export const getAccessToken = (state: MapViewStateTree): string => state[MapReducerKey].accessToken;
-export const getAccessTokenError = (state: MapViewStateTree): Error | undefined => state[MapReducerKey].error;
+export const getAccessToken = (state: MapViewStateTree): string => state[MapReducerKey]?.accessToken;
+export const getAccessTokenError = (state: MapViewStateTree): Error | undefined => state[MapReducerKey]?.error;
