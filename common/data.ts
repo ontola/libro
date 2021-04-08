db2675dccae95b1248cdd9759a3e3d858c70f62d
Arthur Dingemans <arthur@argu.co>
2021-04-08 18:27:21 +0200

    core#484 Add pdf as data extension

--- common/data.ts
+++ common/data.ts
@@ -1,1 +1,1 @@
-export const dataExtensions = ['json', 'nq', 'nt', 'n3', 'rdf', 'ttl', 'png', 'hndjson', 'csv'];
+export const dataExtensions = ['json', 'nq', 'nt', 'n3', 'rdf', 'ttl', 'png', 'hndjson', 'csv', 'pdf'];
