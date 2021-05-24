e52c09949a872a48ea506f12274b03595a9f291d
Arthur Dingemans <arthur@argu.co>
2021-05-24 10:46:01 +0200

    core#12 Don't store initial values on mount

--- app/hooks/useStoredState.ts
+++ app/hooks/useStoredState.ts
@@ -7,16 +7,17 @@ const useStoredState = <T = string>(
   parseFromString: (x: string | null) => T = (x: unknown) => x as T,
   parseToString: (x: T) => string = (x: unknown) => x as string,
 ): [T | undefined, Dispatch<SetStateAction<T | undefined>>] => {
-  const [stored, setValue] = React.useState<T | undefined>(storage.getItem(key) !== null ? parseFromString(storage.getItem(key)) : initialValue);
-
-  React.useEffect(() => {
-    if (stored !== undefined) {
-      storage.setItem(key, parseToString(stored));
+  const resolvedInitialValue = storage.getItem(key) !== null ? parseFromString(storage.getItem(key)) : initialValue;
+  const [stored, setValueRaw] = React.useState<T | undefined>(resolvedInitialValue);
+  const setValue = React.useCallback((value) => {
+    if (value !== undefined) {
+      storage.setItem(key, parseToString(value));
     } else {
       storage.removeItem(key);
     }
-    setValue(stored);
-  }, [stored]);
+
+    setValueRaw(value);
+  }, [key, setValueRaw, storage, parseToString]);
 
   return [stored, setValue];
 };
