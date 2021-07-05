ac002e9cd52a12a3cb703d167225e5da0fb6b360
Arthur Dingemans <arthur@argu.co>
2021-07-05 15:51:12 +0200

    core#12 Catch invalid url

--- common/data.js
+++ common/data.js
@@ -1,7 +1,11 @@
 export const dataExtensions = ['.json', '.nq', '.nt', '.n3', '.rdf', '.ttl', '.png', '.hndjson', '.csv', '.pdf'];
 
 export const hasDataExtension = (url) => {
-  const pUrl = new URL(url);
+  try {
+    const pUrl = new URL(url);
 
-  return dataExtensions.some((x) => pUrl.pathname.endsWith(x));
+    return dataExtensions.some((x) => pUrl.pathname.endsWith(x));
+  } catch {
+    return false;
+  }
 };
