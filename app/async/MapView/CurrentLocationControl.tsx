5438e66bfea7cb4ab986e306c57ef88555ac5101
Job Eijdems <job@argu.co>
2021-08-18 14:49:31 +0200

    core#321 Refactor to functional component

--- app/async/MapView/CurrentLocationControl.tsx
+++ app/async/MapView/CurrentLocationControl.tsx
@@ -1,129 +1,113 @@
 import MyLocationIcon from '@material-ui/icons/MyLocation';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import clsx from 'clsx';
-import { Geolocation } from 'ol';
+import { Geolocation, Map } from 'ol';
 import { Control } from 'ol/control';
 import { CLASS_CONTROL, CLASS_UNSELECTABLE } from 'ol/css';
-import EventType from 'ol/events/EventType';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class CurrentLocationControl extends Control {
-  private static geolocation?: Geolocation;
-  private readonly positionLayer: VectorLayer;
-  private readonly button: HTMLButtonElement;
+const DEFAULT_ZOOM = 13;
 
-  constructor(tipLabel: string) {
-    super({
-      element: document.createElement('div'),
-    });
+interface CurrentLocationButtonProps {
+  getMap: () => Map,
+  tooltip: string,
+}
 
-    this.positionLayer = new VectorLayer({
-      source: new VectorSource({
-        features: [],
-      }),
-    });
+const CurrentLocationButton = ({
+  getMap,
+  tooltip,
+}: CurrentLocationButtonProps) => {
+  const map = getMap();
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [geolocation, setGeolocation] = React.useState<Geolocation | undefined>(undefined);
 
-    const button = document.createElement('button');
-    button.setAttribute('type', 'button');
-    button.title = tipLabel;
-    this.button = button;
-    this.setNormalIcon();
-    button.addEventListener(
-      EventType.CLICK,
-      this.handleClick.bind(this),
-      false,
-    );
+  const panToLocation = (location: Geolocation): void => {
+    const coordinates = location?.getPosition();
 
-    this.element.className = clsx('current-location', CLASS_UNSELECTABLE, CLASS_CONTROL);
-    this.element.appendChild(button);
+    if (coordinates) {
+      const view = map?.getView();
+      const zoom = view?.getZoom() || DEFAULT_ZOOM;
+      view?.animate({
+        center: coordinates,
+        duration: 300,
+        zoom: Math.max(zoom, DEFAULT_ZOOM),
+      });
+    }
+  };
 
-    this.getMap = this.getMap.bind(this);
-    this.panToCurrentLocation = this.panToCurrentLocation.bind(this);
-  }
-
-  public start(): void {
-    const map = this.getMap();
-
-    this.setLoadingIcon();
+  const start = (): void => {
+    setLoading(true);
 
     if (!map) {
       return;
     }
 
-    map.addLayer(this.positionLayer);
-
-    if (!CurrentLocationControl.geolocation) {
-      CurrentLocationControl.geolocation = new Geolocation({
+    if (!geolocation) {
+      const newGeoLocation = new Geolocation({
         projection: map.getView().getProjection(),
         trackingOptions: {
           enableHighAccuracy: true,
         },
       });
-      CurrentLocationControl.geolocation.setTracking(true);
-      CurrentLocationControl.geolocation.once('change', () => {
-        this.setNormalIcon();
-        this.panToCurrentLocation();
+      newGeoLocation.setTracking(true);
+      newGeoLocation.once('change', () => {
+        setLoading(false);
+        newGeoLocation.setTracking(false);
+        panToLocation(newGeoLocation);
       });
-    } else {
-      this.panToCurrentLocation();
+      setGeolocation(newGeoLocation);
     }
-  }
 
-  public stop(): void {
-    this.getMap()?.removeLayer(this.positionLayer!);
+  };
 
-    if (CurrentLocationControl.geolocation) {
-
-      if (!CurrentLocationControl.geolocation.getListeners('change:position')?.length) {
-        CurrentLocationControl.geolocation.setTracking(false);
-        CurrentLocationControl.geolocation = undefined;
-      }
-    }
-  }
-
-  public panToCurrentLocation(): void {
-    const coordinate = CurrentLocationControl.geolocation?.getPosition();
-    const defaultZoom = 13;
-
-    if (coordinate) {
-      const view = this.getMap()?.getView();
-      const zoom = view?.getZoom() || defaultZoom;
-      view?.animate({
-        center: coordinate,
-        duration: 300,
-        zoom: Math.max(zoom, defaultZoom),
-      });
-    }
-  }
-
-  protected setLoadingIcon(): void {
-    ReactDOM.render(
-      <CircularProgress
-        style={{ verticalAlign: 'middle' }}
-      />,
-      this.button);
-  }
-
-  protected setNormalIcon(): void {
-    ReactDOM.render(
-      <MyLocationIcon
-        fontSize="small"
-        style={{ verticalAlign: 'middle' }}
-      />,
-      this.button);
-  }
-
-  protected handleClick(event: MouseEvent): void {
+  const handleClick: React.MouseEventHandler = (event): void => {
     event.preventDefault();
 
-    if (CurrentLocationControl.geolocation) {
-      this.panToCurrentLocation();
+    if (geolocation) {
+      panToLocation(geolocation);
     } else {
-      this.start();
+      start();
     }
+  };
+
+  const className = clsx('current-location', CLASS_UNSELECTABLE, CLASS_CONTROL);
+
+  return (
+    <div className={className}>
+      <button
+        title={tooltip}
+        type="button"
+        onClick={handleClick}
+      >
+        {loading ? (
+          <CircularProgress
+            style={{ verticalAlign: 'middle' }}
+          />
+        ) : (
+          <MyLocationIcon
+            fontSize="small"
+            style={{ verticalAlign: 'middle' }}
+          />
+        )}
+      </button>
+    </div>
+  );
+};
+
+class CurrentLocationControl extends Control {
+  constructor(tooltip: string) {
+    super({
+      element: document.createElement('div'),
+    });
+
+    ReactDOM.render(
+      <CurrentLocationButton
+        getMap={this.getMap.bind(this)}
+        tooltip={tooltip}
+      />,
+      this.element,
+    );
   }
 }
 
