d0cad38d7c8c4a082050f61d5a696ff93b9ca019
Thom van Kalkeren <thom@argu.co>
2021-10-05 13:45:35 +0200

    dexpod#81 Live update date fields

--- app/components/RelativeDate/index.tsx
+++ app/components/RelativeDate/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { FormattedRelativeTime, useIntl } from 'react-intl';
 
 import { relativeTimeDestructure } from '../../helpers/date';
+import { filterUpdateInterval, useDateUpdateInterval } from '../../hooks/useDateUpdateInterval';
 import { DATE_FORMAT } from '../DetailDate';
 
 export interface RelativeDateProps {
@@ -15,6 +16,7 @@ const RelativeDate = ({
   title,
 }: RelativeDateProps): JSX.Element | null => {
   const intl = useIntl();
+  const interval = useDateUpdateInterval();
 
   if (!date) {
     return null;
@@ -22,10 +24,14 @@ const RelativeDate = ({
 
   const dateObj = new Date(date.value);
   const renderTitle = title || intl.formatTime(dateObj, DATE_FORMAT);
+  const timeStructure = relativeTimeDestructure(dateObj.getTime());
 
   return (
     <div title={renderTitle}>
-      <FormattedRelativeTime {...relativeTimeDestructure(dateObj.getTime())} />
+      <FormattedRelativeTime
+        updateIntervalInSeconds={filterUpdateInterval(timeStructure.unit, interval)}
+        {...timeStructure}
+      />
     </div>
   );
 };
