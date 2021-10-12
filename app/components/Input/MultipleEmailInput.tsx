aeeff41b66e76978caa400e577eda81bdd50d881
Thom van Kalkeren <thom@argu.co>
2021-10-13 00:48:55 +0200

    core#843 Refactor MultipleEmailInput

--- app/components/Input/MultipleEmailInput.tsx
+++ app/components/Input/MultipleEmailInput.tsx
@@ -1,10 +1,10 @@
 import TextField from '@material-ui/core/TextField';
 import Autocomplete, { AutocompleteChangeReason } from '@material-ui/lab/Autocomplete';
+import { AutocompleteInputChangeReason } from '@material-ui/lab/useAutocomplete/useAutocomplete';
 import { makeStyles } from '@material-ui/styles';
 import rdf from '@ontologies/core';
 import React from 'react';
 import { useIntl } from 'react-intl';
-import { useDebouncedCallback } from 'use-debounce';
 
 import { ShapeForm } from '../../hooks/useShapeProps';
 import { LibroTheme } from '../../themes/themes';
@@ -12,8 +12,6 @@ import { formMessages } from '../../translations/messages';
 import { InputComponentProps } from '../FormField/InputComponentProps';
 
 import HiddenRequiredInput from './HiddenRequiredInput';
-
-const REGEX_TEST_DEBOUNCE_TIME = 1000;
 
 const useOverrideStyle = makeStyles({
   inputFocused: {
@@ -36,31 +34,196 @@ const useStyles = makeStyles<LibroTheme>((theme) => ({
 }));
 
 enum EmailError {
-  NO_ERROR = 0,
+  NO_ERROR,
   MAX_LENGTH,
   INVALID_EMAIL,
   MAX_EMAIL_COUNT,
 }
 
-type ErrorMessages = (fieldShape: ShapeForm) => Record<EmailError, (current: number) => string>;
+interface ErrorMessageMap {
+  [k: string]: (report: ValidationReport) => string;
+}
+
+type ErrorMessages = (fieldShape: ShapeForm) => ErrorMessageMap;
 
 const matchMultiple = /\n|\s|,\s*/gm;
+
+const multipleValueMapper = (val: string) => val.split(matchMultiple);
+
+const filterUnique = (list: string[]) => Array.from(new Set(list));
+
+interface ValidationResult {
+  valid: string[];
+  invalid: string[];
+}
+
+const indicativeName = (reason: AutocompleteChangeReason): boolean =>
+  reason !== 'create-option' && reason !== 'blur';
+
+const validateValues = (pattern: RegExp, emails: string[]): ValidationResult => {
+  const flattenedValues = filterUnique(emails.flatMap(multipleValueMapper));
+  const results: ValidationResult = {
+    invalid: [],
+    valid: [],
+  };
+
+  for (const value of flattenedValues) {
+    const isValid = pattern.test(value);
+
+    if (isValid) {
+      results.valid.push(value);
+    } else {
+      results.invalid.push(value);
+    }
+  }
+
+  return results;
+};
+
+const containsInvalidEmail = (report: ValidationResult): boolean => report.invalid.length === 0;
 
 const useErrorMessages: ErrorMessages = ({ maxCount, maxLength }) => {
   const intl = useIntl();
 
   return {
-    [EmailError.NO_ERROR]: () => '',
-    [EmailError.MAX_LENGTH]: (length: number) => intl.formatMessage(formMessages.maxEmailLengthMessage, {
-      length,
-      maxLength,
-    }),
+    [EmailError.NO_ERROR]: (_: NoErrorReport) => '',
+    [EmailError.MAX_LENGTH]: (report: MaxLengthReport) => intl.formatMessage(
+      formMessages.maxEmailLengthMessage,
+      {
+        length: report.errorData,
+        maxLength,
+      },
+    ),
     [EmailError.INVALID_EMAIL]: () => intl.formatMessage(formMessages.invalidEmailMessage),
-    [EmailError.MAX_EMAIL_COUNT]: (count: number) => intl.formatMessage(formMessages.maxEmailCountMessage, {
-      count,
-      maxCount,
-    }),
+    [EmailError.MAX_EMAIL_COUNT]: (report: MaxEmailCountReport) => intl.formatMessage(
+      formMessages.maxEmailCountMessage,
+      {
+        count: report.errorData,
+        maxCount,
+      },
+    ),
+  } as ErrorMessageMap;
+};
+
+interface ValidatorConfig {
+  pattern: RegExp,
+  maxCount: number,
+  maxLength: number,
+}
+
+interface NoErrorReport {
+  error: EmailError.NO_ERROR;
+}
+
+interface InvalidEmailReport {
+  error: EmailError.INVALID_EMAIL;
+  errorData: ValidationResult;
+}
+
+interface MaxEmailCountReport {
+  error: EmailError.MAX_EMAIL_COUNT;
+  errorData: number;
+}
+
+interface MaxLengthReport {
+  error: EmailError.MAX_LENGTH;
+  errorData: number;
+}
+
+type ValidationReport = NoErrorReport |
+  InvalidEmailReport |
+  MaxEmailCountReport |
+  MaxLengthReport;
+
+const patternToRegExp = (pattern: string): RegExp => new RegExp(pattern!.replace(/\s/g, ''), 'mu');
+
+const validatorConfigFromShape = (fieldShape: ShapeForm): ValidatorConfig => {
+  if (!fieldShape.pattern) {
+    throw new Error('Shape has no pattern.');
+  }
+
+  return {
+    maxCount: fieldShape.maxCount ?? Infinity,
+    maxLength: fieldShape.maxLength ?? Infinity,
+    pattern: patternToRegExp(fieldShape.pattern),
   };
+};
+
+const validateEmails = (
+  validator: ValidatorConfig,
+  emails: string[],
+): ValidationReport => {
+  const emailCount = emails.length;
+  const inputLength = emails.reduce((acc, e) => acc + e.length, 0);
+  const validationResult = validateValues(validator.pattern, emails);
+
+  if (emailCount > validator.maxCount) {
+    return {
+      error: EmailError.MAX_EMAIL_COUNT,
+      errorData: emails.length,
+    };
+  } else if (inputLength > validator.maxLength) {
+    return {
+      error: EmailError.MAX_LENGTH,
+      errorData: inputLength,
+    };
+  } else if (containsInvalidEmail(validationResult)) {
+    return {
+      error: EmailError.INVALID_EMAIL,
+      errorData: validationResult,
+    };
+  }
+
+  return {
+    error: EmailError.NO_ERROR,
+  };
+};
+
+const validateInput = (
+  validator: ValidatorConfig,
+  textFieldValue: string,
+): ValidationReport => {
+  const indicatedDone = textFieldValue.endsWith(' ');
+  const addedOption = textFieldValue.trim();
+
+  if (indicatedDone && !validator.pattern.test(addedOption)) {
+    return {
+      error: EmailError.INVALID_EMAIL,
+      errorData: {
+        invalid: [addedOption],
+        valid: [],
+      },
+    };
+  }
+
+  return {
+    error: EmailError.NO_ERROR,
+  };
+};
+
+const validate = (
+  validator: ValidatorConfig,
+  emails: string[],
+  textFieldValue: string,
+): ValidationReport => {
+  const enteredEmails = validateEmails(validator, emails);
+  const input = validateInput(validator, textFieldValue);
+
+  if (input.error === EmailError.INVALID_EMAIL) {
+    if (enteredEmails.error === EmailError.INVALID_EMAIL) {
+      return {
+        error: EmailError.INVALID_EMAIL,
+        errorData: {
+          invalid: input.errorData.invalid.concat(enteredEmails.errorData.invalid),
+          valid: input.errorData.valid.concat(enteredEmails.errorData.valid),
+        },
+      };
+    }
+
+    return input;
+  }
+
+  return enteredEmails;
 };
 
 export const MultipleEmailInput = ({
@@ -71,114 +234,98 @@ export const MultipleEmailInput = ({
   fieldShape,
   values,
 }: InputComponentProps): JSX.Element => {
-  const { pattern } = fieldShape;
-  const reg = new RegExp(pattern!.replace(/\s/g, ''), 'mu');
-
-  const [emails, setEmails] = React.useState<string[]>(values.map((v) => v?.value).filter(Boolean));
-  const [textFieldValue, setTextFieldValue] = React.useState<string>('');
-
-  const [{ error, errorData }, setError] = React.useState({
-    error: EmailError.NO_ERROR,
-    errorData: 0,
-  });
-
-  const [debouncedRegTester] = useDebouncedCallback((val: string) => {
-    reg.test(val) && setError({
-      error: EmailError.NO_ERROR,
-      errorData: 0,
-    });
-  }, REGEX_TEST_DEBOUNCE_TIME);
-
   const overrideClasses = useOverrideStyle();
   const classes = useStyles();
   const errorMessages = useErrorMessages(fieldShape);
 
-  React.useEffect(() => {
-    if (emails.length > (fieldShape?.maxCount ?? Infinity)) {
-      setError({
-        error: EmailError.MAX_EMAIL_COUNT,
-        errorData: emails.length,
-      });
+  const [textFieldValue, setTextFieldValue] = React.useState<string>('');
+  const [reason, setReason] = React.useState<AutocompleteChangeReason>('create-option');
+  const [emails, setEmails] = React.useState<string[]>(values.map((v) => v?.value).filter(Boolean));
 
-      return;
-    }
+  const [validatorConfig] = React.useState(() => validatorConfigFromShape(fieldShape));
+  const [report, setReport] = React.useState(() => validate(validatorConfig, emails, textFieldValue));
 
-    const totalLength = emails.join('').length;
+  const hasError = report.error !== EmailError.NO_ERROR;
 
-    if (totalLength > (fieldShape.maxLength ?? Infinity)) {
-      setError({
-        error: EmailError.MAX_LENGTH,
-        errorData: totalLength,
-      });
-
-      return;
-    }
-
-    setError({
-      error: EmailError.NO_ERROR,
-      errorData: 0,
-    });
-
-    onChange(emails.map((v) => rdf.literal(v)));
-  }, [emails]);
-
-  const valueFilter = (val: string) => reg.test(val);
-  const multipleValueMapper = (val: string) => val.split(matchMultiple);
-  const filterUnique = (list: string[]) => Array.from(new Set(list));
-
-  const handleNewValue = (newValues: string[], reason: AutocompleteChangeReason = 'create-option') => {
-    const nextEmails = reason === 'create-option'
-      ? filterUnique(newValues.flatMap((v) => valueFilter(v) ? multipleValueMapper(v) : []))
-      : newValues;
-
-    setEmails(nextEmails);
-  };
-
-  const handleChange = (e: React.ChangeEvent<unknown>, chips: string[], reason: AutocompleteChangeReason) => {
+  const handleInputChange = (
+    e: React.ChangeEvent<unknown>,
+    value: string,
+    _: AutocompleteInputChangeReason,
+  ) => {
     e.preventDefault();
-    handleNewValue(chips, reason);
+
+    setTextFieldValue(value);
   };
 
-  const handleTextFieldChange = (_: React.ChangeEvent<unknown>, val: string) => {
-    if (error === EmailError.MAX_LENGTH || error === EmailError.MAX_EMAIL_COUNT) {
-      return;
-    }
+  const handleChange = (
+    e: React.ChangeEvent<unknown>,
+    chips: string[],
+    changeReason: AutocompleteChangeReason,
+  ) => {
+    e.preventDefault();
 
-    const trimmed = val.trim();
+    setReason(changeReason);
+    setEmails(chips);
+  };
 
-    if (val.endsWith(' ') || matchMultiple.test(trimmed)) {
-      if (reg.test(trimmed)) {
-        handleNewValue([...emails, trimmed]);
+  React.useEffect(() => {
+    const nextReport = validate(validatorConfig, emails, textFieldValue);
+
+    setReport(nextReport);
+  }, [validatorConfig, emails]);
+
+  React.useEffect(() => {
+    // TODO: Something with reason and indicativeName
+
+    switch (report.error) {
+    case EmailError.MAX_EMAIL_COUNT:
+    case EmailError.MAX_LENGTH:
+      // Don't update emails
+      break;
+
+    case EmailError.NO_ERROR:
+      if (textFieldValue.length > 0) {
+        setEmails([...emails, textFieldValue]);
         setTextFieldValue('');
-
-        return;
       }
 
-      setError({
-        error: EmailError.INVALID_EMAIL,
-        errorData: 0,
+      break;
+
+    case EmailError.INVALID_EMAIL:
+      // TODO: debouncedRegTester(trimmed);
+
+      setEmails(report.errorData.valid);
+      // HACK: Autocomplete clears textinput value on change without a way to disable it.
+      //   Wrapping the setState in a timeout fixes this.
+      setTimeout(() => {
+        setTextFieldValue(report.errorData.invalid.join(','));
       });
+      break;
     }
+  }, [report]);
 
-    if (error === EmailError.INVALID_EMAIL) {
-      debouncedRegTester(trimmed);
+  React.useEffect(() => {
+    if (report.error === EmailError.NO_ERROR) {
+      onChange(emails.map((v) => rdf.literal(v)));
     }
+  }, [emails, report]);
 
-    setTextFieldValue(trimmed);
+  // RENDER
 
+  const renderErrorMessage = () => {
+    const test = errorMessages[report.error];
+
+    return (
+      <span className={classes.errorMessage}>
+        {test(report)}
+      </span>
+    );
   };
-
-  const renderErrorMessage = () => (
-    <span className={classes.errorMessage}>
-      {errorMessages[error](errorData)}
-    </span>
-  );
-
-  const hasError = error !== EmailError.NO_ERROR;
 
   return (
     <React.Fragment>
       <Autocomplete
+        autoSelect
         freeSolo
         multiple
         ChipProps={{ color: 'primary' }}
@@ -206,7 +353,7 @@ export const MultipleEmailInput = ({
         )}
         value={emails}
         onChange={handleChange}
-        onInputChange={handleTextFieldChange}
+        onInputChange={handleInputChange}
       />
       {fieldShape?.required && (
         <HiddenRequiredInput
