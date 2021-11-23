101a0d07ce2a1201df90b1450c8387543379be53
Arthur Dingemans <arthur@argu.co>
2021-11-23 11:18:35 +0100

    dexpods#96 Add option to publish custom dataset

--- app/views/Dexes/Distribution/DistributionCard.tsx
+++ app/views/Dexes/Distribution/DistributionCard.tsx
@@ -2,35 +2,55 @@ import * as dcterms from '@ontologies/dcterms';
 import {
   Property,
   register,
+  useGlobalIds,
 } from 'link-redux';
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 
+import Button from '../../../components/Button';
 import CardContent from '../../../components/Card/CardContent';
 import CardDivider from '../../../components/Card/CardDivider';
 import Heading, { HeadingSize } from '../../../components/Heading';
+import { isDifferentWebsite } from '../../../helpers/iris';
 import dcat from '../../../ontology/dcat';
 import dexes from '../../../ontology/dexes';
 import { cardTopology } from '../../../topologies/Card';
 import { cardFixedTopology } from '../../../topologies/Card/CardFixed';
 import { cardMainTopology } from '../../../topologies/Card/CardMain';
 import { inlineTopology } from '../../../topologies/Inline';
+import { messages } from '../messages';
 
-const DistributionCard = () => (
-  <CardContent endSpacing>
-    <CardDivider margin />
-    <Heading size={HeadingSize.SM}>
-      <Property
-        label={dcterms.format}
-        topology={inlineTopology}
-      />
-    </Heading>
-    <Property
-      label={dcat.accessURL}
-      topology={inlineTopology}
-    />
-    <Property label={dexes.offer} />
-  </CardContent>
-);
+const DistributionCard = () => {
+  const [accessUrl] = useGlobalIds(dcat.accessURL);
+
+  return (
+    <CardContent endSpacing>
+      <CardDivider margin />
+      <Heading size={HeadingSize.SM}>
+        <Property
+          label={dcterms.format}
+          topology={inlineTopology}
+        />
+      </Heading>
+      {isDifferentWebsite(accessUrl) ? (
+        <div>
+          <Button
+            href={accessUrl.value}
+            icon="external-link"
+          >
+            <FormattedMessage {...messages.goToFile} />
+          </Button>
+        </div>
+      ) : (
+        <Property
+          label={dcat.accessURL}
+          topology={inlineTopology}
+        />
+      )}
+      <Property label={dexes.offer} />
+    </CardContent>
+  );
+};
 
 DistributionCard.type = dexes.Distribution;
 
--- app/views/Dexes/messages.ts
+++ app/views/Dexes/messages.ts
@@ -3,12 +3,16 @@ import { MessageDescriptor, defineMessages } from 'react-intl';
 type Messages = { [key: string]: MessageDescriptor };
 
 export const messages: Messages = defineMessages({
+  goToFile: {
+    defaultMessage: 'Go to file',
+    id: 'https://dexpods.eu/i18n/goToFile',
+  },
   showInBroker: {
-    defaultMessage: 'Bekijk in de Broker',
+    defaultMessage: 'Show in the Broker',
     id: 'https://dexpods.eu/i18n/showInBroker',
   },
   showInDexes: {
-    defaultMessage: 'Bekijk op Dexes',
+    defaultMessage: 'Show in Dexes',
     id: 'https://dexpods.eu/i18n/showInDexes',
   },
 });
