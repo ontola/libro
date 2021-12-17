798f2716725b67d12b1676e7224717aa55d347d8
Job Eijdems <job@argu.co>
2021-12-17 15:10:16 +0100

    core#863 Refactor CoverImage.scss to Jss

--- app/components/CoverImage/index.tsx
+++ app/components/CoverImage/index.tsx
@@ -1,11 +1,54 @@
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
-import './CoverImage.scss';
+import { LibroTheme, Margin } from '../../themes/themes';
+import { cardClassIdentifier, cardFixedClassIdentifier } from '../../topologies/Card/sharedCardStyles';
 
 interface PropTypes {
   positionY: number | string;
   url: string;
 }
+
+const useStyles = makeStyles((theme: LibroTheme) => ({
+  coverImageChild: {
+    [`.${cardClassIdentifier} &`]: {
+      filter: 'none',
+      opacity: 1,
+      transform: 'none',
+    },
+    backgroundSize: 'cover',
+    filter: 'blur(15px)',
+    height: '100%',
+    opacity: '.3',
+    transform: 'scale(1.2)',
+    width: '100%',
+  },
+  coverImageWrapper : {
+    [`.${cardClassIdentifier} &`] :{
+      borderTopLeftRadius: theme.shape.borderRadius,
+      borderTopRightRadius: theme.shape.borderRadius,
+      height: '7em',
+      overflow: 'hidden',
+      position: 'relative',
+      width: '100%',
+    },
+    [`.${cardFixedClassIdentifier} &`]: {
+      display: 'flex',
+      flex: 1,
+      height: 'auto',
+    },
+    '.Page > &' : {
+      marginTop: `-${theme.spacing(Margin.Medium)}`,
+    },
+    backgroundColor: theme.palette.grey.xxLight,
+    height: '19em',
+    left: 0,
+    overflow: 'hidden',
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+}));
 
 /**
  * Just a wrapper component
@@ -14,17 +57,21 @@ interface PropTypes {
 const CoverImage: React.FC<PropTypes> = ({
   url,
   positionY,
-}) => (
-  <div className="CoverImage__wrapper">
-    <div
-      className="CoverImage__child"
-      data-testid="coverImage"
-      style={{
-        backgroundImage: `url(${url})`,
-        backgroundPositionY: positionY ? `${positionY}%` : undefined,
-      }}
-    />
-  </div>
-);
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.coverImageWrapper}>
+      <div
+        className={classes.coverImageChild}
+        data-testid="coverImage"
+        style={{
+          backgroundImage: `url(${url})`,
+          backgroundPositionY: positionY ? `${positionY}%` : undefined,
+        }}
+      />
+    </div>
+  );
+};
 
 export default CoverImage;
