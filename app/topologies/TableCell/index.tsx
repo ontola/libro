d279ce118e02c4b18cccf17d4b0c1c237b417bbe
Job Eijdems <job@argu.co>
2021-12-17 15:58:15 +0100

    core#863 Refactor TableCell.scss to Jss

--- app/topologies/TableCell/index.tsx
+++ app/topologies/TableCell/index.tsx
@@ -1,16 +1,30 @@
+import { createStyles } from '@material-ui/core';
+import { WithStyles, withStyles } from '@material-ui/styles';
 import { TopologyProvider } from 'link-redux';
 import PropTypes from 'prop-types';
+import { ReactNode } from 'react';
 
+import { headingCID } from '../../components/Heading';
 import argu from '../../ontology/argu';
-
-import './TableCell.scss';
+import { LibroTheme, Margin } from '../../themes/themes';
 
 export const tableCellTopology = argu.ns('tableCell');
 
-interface PropTypes {
+const styles = (theme: LibroTheme) => createStyles({
+  tableCell: {
+    [`& .${headingCID}`]: {
+      wordBreak: 'normal',
+    },
+    padding: `${theme.spacing(Margin.Small)} ${theme.spacing(Margin.Large)}`,
+    textAlign: 'left',
+  },
+});
+
+type PropTypes = WithStyles<typeof styles> & {
+  children?: ReactNode;
   colspan?: number;
   elementProps?: Record<string, unknown>;
-}
+};
 
 class TableCell extends TopologyProvider<PropTypes> {
   public static propTypes = {
@@ -20,10 +34,10 @@ class TableCell extends TopologyProvider<PropTypes> {
   constructor(props: PropTypes) {
     super(props);
 
-    this.className = 'TableCell';
+    this.className = this.props.classes.tableCell;
     this.elementType = 'td';
     this.topology = tableCellTopology;
   }
 }
 
-export default TableCell;
+export default withStyles(styles)(TableCell);
