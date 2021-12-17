ca2bd013907ae58be4923470e2d863f6e6bf266e
Job Eijdems <job@argu.co>
2021-12-17 15:06:17 +0100

    core#863 Refactor GridHeader.scss to Jss

--- app/components/Grid/GridHeader.tsx
+++ app/components/Grid/GridHeader.tsx
@@ -1,13 +1,21 @@
+import { makeStyles } from '@material-ui/styles';
 import React, { ReactElement } from 'react';
 
 import ContainerFloat from '../../topologies/Container/ContainerFloat';
-
-import './GridHeader.scss';
 
 interface PropTypes {
   /** The header floats to the top left */
   header: ReactElement;
 }
+
+const useStyles = makeStyles({
+  gridHeader: {
+    display: 'flex',
+  },
+  header: {
+    flex: 1,
+  },
+});
 
 /**
  * Holds a header and menu items that float to the top right of the container
@@ -16,17 +24,21 @@ interface PropTypes {
 const GridHeader: React.FC<PropTypes> = ({
   children,
   header,
-}) => (
-  <div className="GridHeader">
-    {header && (
-      <div className="GridHeader--header">
-        {header}
-      </div>
-    )}
-    <ContainerFloat>
-      {children}
-    </ContainerFloat>
-  </div>
-);
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.gridHeader}>
+      {header && (
+        <div className={classes.header}>
+          {header}
+        </div>
+      )}
+      <ContainerFloat>
+        {children}
+      </ContainerFloat>
+    </div>
+  );
+};
 
 export default GridHeader;
