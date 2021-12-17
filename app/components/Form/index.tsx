0a45476a5fbeb7fd3d03172f7f36281b85d23a4d
Job Eijdems <job@argu.co>
2021-12-17 15:08:19 +0100

    core#863 Remove unused Form.scss

--- app/components/Form/index.tsx
+++ app/components/Form/index.tsx
@@ -1,5 +1,3 @@
-import './Form.scss';
-
 export { default as FormSection } from './FormSection';
 export { default as FormFooterImage } from './FooterImage';
 export { default as FormFooterRight } from './FooterRight';
