4baf34e9dd549b417eccc9bf5d73a0605c75f922
Job Eijdems <job@argu.co>
2021-12-17 15:55:15 +0100

    core#863 Refactor TableHead.scss to Jss

--- app/topologies/TableHead/index.tsx
+++ app/topologies/TableHead/index.tsx
@@ -1,24 +1,45 @@
+import { WithStyles, withStyles } from '@material-ui/styles';
 import { TopologyProvider } from 'link-redux';
 import PropTypes from 'prop-types';
+import { PropsWithChildren } from 'react';
 
 import argu from '../../ontology/argu';
-
-import './TableHead.scss';
+import { LibroTheme } from '../../themes/themes';
 
 export const tableHeadTopology = argu.ns('tableHead');
 
-class TableHead extends TopologyProvider {
+const styles = (theme: LibroTheme) => ({
+  tableHead: {
+    '& button': {
+      cursor: 'pointer',
+    },
+    '& tr > th': {
+      '&:first-child': {
+        borderTopLeftRadius: theme.shape.borderRadius,
+      },
+      '&:last-child': {
+        borderTopRightRadius: theme.shape.borderRadius,
+      },
+      backgroundColor: theme.palette.grey.xxLight,
+    },
+    color: theme.palette.grey.midDark,
+  },
+});
+
+type TableHeadProps = PropsWithChildren<WithStyles<typeof styles>>;
+
+class TableHead extends TopologyProvider<TableHeadProps> {
   public static propTypes = {
     children: PropTypes.node.isRequired,
   };
 
-  constructor(props: Record<string, unknown>) {
+  constructor(props: TableHeadProps) {
     super(props);
 
-    this.className = 'TableHead';
+    this.className = this.props.classes.tableHead;
     this.elementType = 'thead';
     this.topology = tableHeadTopology;
   }
 }
 
-export default TableHead;
+export default withStyles(styles)(TableHead);
