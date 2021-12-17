cdc1a609657bd7c8458a50e841cbebf4cd8411bb
Job Eijdems <job@argu.co>
2021-12-17 15:49:47 +0100

    core#863 Refactor AttributeList.scss to Jss

--- app/topologies/AttributeList/index.tsx
+++ app/topologies/AttributeList/index.tsx
@@ -1,15 +1,55 @@
+import { createStyles } from '@material-ui/core';
+import { WithStyles, withStyles } from '@material-ui/styles';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 import argu from '../../ontology/argu';
+import { LibroTheme } from '../../themes/themes';
 import Topology, { TopologyContent } from '../Topology';
-
-import './AttributeList.scss';
 
 export const attributeListTopology = argu.ns('attributeList');
 
-class AttributeList extends Topology {
+const styles = (theme: LibroTheme) => createStyles({
+  attributeList: {
+    '& a': {
+      '&:hover': {
+        textDecoration: 'underline',
+      },
+      display: 'block',
+      fontWeight: 'normal',
+      textDecoration: 'underline',
+    },
+
+    '& label': {
+      color: theme.palette.grey.midDark,
+    },
+
+    '& td': {
+      padding: '.3em 0',
+    },
+
+    '& th': {
+      color: theme.palette.grey.midDark,
+      fontWeight: 600,
+      paddingRight: '1rem',
+      textAlign: 'left',
+    },
+
+    '&:not(&--full-label)': {
+      'th': {
+        width: '30%',
+      },
+    },
+    marginBottom: '.6rem',
+  },
+});
+
+type AttributeListProps = WithStyles<typeof styles> & {
+  fullLabel?: boolean;
+};
+
+class AttributeList extends Topology<PropsWithChildren<AttributeListProps>> {
   public static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
@@ -20,7 +60,7 @@ class AttributeList extends Topology {
     fullLabel: true,
   };
 
-  constructor(props: Record<string, unknown>) {
+  constructor(props: AttributeListProps) {
     super(props);
 
     this.elementType = 'table';
@@ -29,8 +69,8 @@ class AttributeList extends Topology {
 
   public getClassName(): string {
     return clsx({
-      'AttributeList': true,
-      'AttributeList--full-label': (this.props as any).fullLabel,
+      [this.props.classes.attributeList]: true,
+      [`${this.props.classes.attributeList}--full-label`]: (this.props as any).fullLabel,
     });
   }
 
@@ -49,4 +89,4 @@ class AttributeList extends Topology {
   }
 }
 
-export default AttributeList;
+export default withStyles(styles)(AttributeList);
