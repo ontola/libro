c949225e72719cca9b7d946d5e98da11eb5b2d0a
Job Eijdems <job@argu.co>
2021-12-21 20:54:26 +0100

    core#863 Refactor MenuSection.scss to Jss

--- app/components/MenuSectionLabel/index.tsx
+++ app/components/MenuSectionLabel/index.tsx
@@ -1,24 +1,53 @@
+import { makeStyles } from '@material-ui/styles';
 import { LinkedPropType } from 'link-redux';
 import React from 'react';
 
-import './MenuSection.scss';
+import { LibroTheme } from '../../themes/themes';
 
 export interface MenuSectionLabelProps {
   linkedProp: Exclude<LinkedPropType, any[]>;
   name?: React.ReactNode;
 }
 
-const MenuSectionLabel = ({ linkedProp, name }: MenuSectionLabelProps): JSX.Element => (
-  <div
-    className="MenuSectionLabel"
-    data-test="MenuSection-MenuSectionLabel"
-  >
-    <div className="MenuSectionLabel__bar" />
-    <div className="MenuSectionLabel__text">
-      {name ?? linkedProp.value}
+const useStyles = makeStyles<LibroTheme>((theme) => ({
+  menuSectionLabel: {
+    alignItems: 'center',
+    display: 'flex',
+    marginTop: '.7em',
+  },
+  menuSectionLabelBar: {
+    backgroundColor: theme.palette.transparent.light,
+    flex: 1,
+    height: '2px',
+  },
+  menuSectionLabelText: {
+    display: 'block',
+    flex: 1,
+    fontSize: '.8em',
+    fontWeight: 'bold',
+    letterSpacing: '.1em',
+    paddingLeft: '.1em',
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    width: '100%',
+  },
+}));
+
+const MenuSectionLabel = ({ linkedProp, name }: MenuSectionLabelProps): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <div
+      className={classes.menuSectionLabel}
+      data-test="MenuSection-MenuSectionLabel"
+    >
+      <div className={classes.menuSectionLabelBar} />
+      <div className={classes.menuSectionLabelText}>
+        {name ?? linkedProp.value}
+      </div>
+      <div className={classes.menuSectionLabelBar} />
     </div>
-    <div className="MenuSectionLabel__bar" />
-  </div>
-);
+  );
+};
 
 export default MenuSectionLabel;
