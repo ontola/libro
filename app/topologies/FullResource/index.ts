e96b564a6490aad75e228dfa61c42e6ab1ace6ad
Job Eijdems <job@argu.co>
2021-12-22 10:41:23 +0100

    core#863 Refactor FullResource/index.scss to Jss

--- app/topologies/FullResource/index.ts
+++ app/topologies/FullResource/index.ts
@@ -1,23 +1,37 @@
+import {
+  WithStyles,
+  createStyles,
+  withStyles, 
+} from '@material-ui/styles';
 import { TopologyProvider } from 'link-redux';
 import PropTypes from 'prop-types';
+import { PropsWithChildren } from 'react';
 
 import argu from '../../ontology/argu';
-
-import './index.scss';
+import { LibroTheme, Margin } from '../../themes/themes';
 
 export const fullResourceTopology = argu.fullResource;
 
-class FullResource extends TopologyProvider {
+const styles = (theme: LibroTheme) => createStyles({
+  fullResource: {
+    marginTop: theme.spacing(Margin.Medium),
+    position: 'relative',
+  },
+});
+
+type FullResourceProps = PropsWithChildren<WithStyles<typeof styles>>;
+
+class FullResource extends TopologyProvider<FullResourceProps> {
   public static propTypes = {
     children: PropTypes.node.isRequired,
   };
 
-  constructor(props: Record<string, unknown>) {
+  constructor(props: FullResourceProps) {
     super(props);
 
     this.topology = fullResourceTopology;
-    this.className = 'FullResource';
+    this.className = this.props.classes.fullResource;
   }
 }
 
-export default FullResource;
+export default withStyles(styles)(FullResource);
