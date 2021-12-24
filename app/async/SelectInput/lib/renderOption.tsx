9df4e6fbb871ffaf4c0d17c103f566e658eacab9
Job Eijdems <job@argu.co>
2021-12-24 18:43:36 +0100

    core#863 Refactor FormField.scss to Jss chapter 2: renderOption

--- app/async/SelectInput/lib/renderOption.tsx
+++ app/async/SelectInput/lib/renderOption.tsx
@@ -1,12 +1,25 @@
+import { makeStyles } from '@material-ui/styles';
 import { SomeTerm, isLiteral } from '@ontologies/core';
 import { Resource } from 'link-redux';
 import React from 'react';
 
+const useStyles = makeStyles({
+  fieldListElement: {
+    alignItems: 'center',
+    cursor: 'pointer',
+    display: 'flex',
+    flexDirection: 'row',
+    padding: '0 2px',
+  },
+});
+
 export const renderOption = (item: SomeTerm): JSX.Element => {
+  const classes = useStyles();
+
   if (isLiteral(item.termType)) {
     return (
       <option
-        className="Field__list-element"
+        className={classes.fieldListElement}
         key={item.value}
         value={item.value}
       >
