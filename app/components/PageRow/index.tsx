d0a94779c5b76422019cce55d23779d9bd179f6b
Job Eijdems <job@argu.co>
2021-12-24 10:47:01 +0100

    core#863 Refactor PageRow.scss to Jss

--- app/components/PageRow/index.tsx
+++ app/components/PageRow/index.tsx
@@ -1,21 +1,35 @@
+import { makeStyles } from '@material-ui/styles';
 import clsx from 'clsx';
 import React from 'react';
 
-import './PageRow.scss';
+import { LibroTheme } from '../../themes/themes';
 
 export interface PageRowProps {
   children: React.ComponentType;
   white?: boolean;
 }
 
+const useStyles = makeStyles<LibroTheme>((theme) => ({
+  pageRow: {
+    padding: '3em 0',
+  },
+  pageRowWhite: {
+    backgroundColor: theme.palette.common.white,
+    borderBottom: 'solid 1px rgb(230, 230, 230)',
+    borderTop: 'solid 1px rgb(230, 230, 230)',
+  },
+}));
+
 const PageRow = ({ children, white }: PageRowProps): JSX.Element => {
-  const className = clsx({
-    PageRow,
-    'PageRow--white': white,
-  });
+  const classes = useStyles();
 
   return (
-    <div className={className}>
+    <div
+      className={clsx({
+        [classes.pageRow]: true,
+        [classes.pageRowWhite]: white,
+      })}
+    >
       {children}
     </div>
   );
