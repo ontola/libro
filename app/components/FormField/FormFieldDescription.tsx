0f3e4a5158edf3969f3980b4ab051285f7d20fca
Job Eijdems <job@argu.co>
2021-12-24 18:47:07 +0100

    core#863 Refactor FormField.scss to Jss chapter 3: FormFieldDescription

--- app/components/FormField/FormFieldDescription.tsx
+++ app/components/FormField/FormFieldDescription.tsx
@@ -1,18 +1,29 @@
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
 import Markdown from '../Markdown';
 
 import { FormFieldContext } from './FormField';
 
+const useStyles = makeStyles({
+  fieldDescription:{
+    fontSize: '.8rem',
+    marginBottom: '.3rem',
+    marginTop: '-.3rem',
+    whiteSpace: 'pre-wrap',
+  },
+});
+
 const FormFieldDescription: React.FC = () => {
   const { description } = React.useContext(FormFieldContext);
+  const classes = useStyles();
 
   if (!description) {
     return null;
   }
 
   return (
-    <div className="Field__description">
+    <div className={classes.fieldDescription}>
       <Markdown
         noSpacing
         text={description}
