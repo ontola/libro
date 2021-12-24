73b6091fd48fd7393cb0b0af8438a34e1e6be6c8
Job Eijdems <job@argu.co>
2021-12-24 18:39:25 +0100

    core#863 Refactor FormField.scss to Jss chapter 1: HiddenRequiredInput

--- app/components/Input/HiddenRequiredInput.tsx
+++ app/components/Input/HiddenRequiredInput.tsx
@@ -1,3 +1,4 @@
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
 interface PropTypes {
@@ -5,16 +6,32 @@ interface PropTypes {
   value: string;
 }
 
-const HiddenRequiredInput: React.FC<PropTypes> = ({ name, value }) => (
-  <input
-    required
-    className="hidden-field"
-    id={name}
-    name={name}
-    type="text"
-    value={value || ''}
-    onChange={() => null}
-  />
-);
+const useStyles = makeStyles({
+  inputHiddenField: {
+    border: 0,
+    height: '1px',
+    left: 0,
+    margin: '.1em',
+    padding: 0,
+    position: 'absolute',
+    width: '1px',
+  },
+});
+
+const HiddenRequiredInput: React.FC<PropTypes> = ({ name, value }) => {
+  const classes = useStyles();
+
+  return (
+    <input
+      required
+      className={classes.inputHiddenField}
+      id={name}
+      name={name}
+      type="text"
+      value={value || ''}
+      onChange={() => null}
+    />
+  );
+};
 
 export default HiddenRequiredInput;
