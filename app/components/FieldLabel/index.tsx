630e2ba9f4702a17ea1caa4b50a48dfa4013b7f7
Job Eijdems <job@argu.co>
2021-12-27 10:39:57 +0100

    core#863 Refactor FormField.scss to Jss chapter 4: FieldLabel

--- app/components/FieldLabel/index.tsx
+++ app/components/FieldLabel/index.tsx
@@ -1,5 +1,8 @@
+import { makeStyles } from '@material-ui/styles';
 import clsx from 'clsx';
 import React from 'react';
+
+import { LibroTheme } from '../../themes/themes';
 
 export const fieldLabelCID = 'CID-FieldLabel';
 
@@ -10,27 +13,40 @@ interface PropTypes {
   required?: boolean;
 }
 
+const useStyles = makeStyles<LibroTheme>((theme) => ({
+  fieldLabel: {
+    color: theme.palette.grey.xxLightForegroundSmall,
+    display: 'block',
+    fontSize: '1em',
+    fontWeight: 'bold',
+  },
+}));
+
 const FieldLabel: React.FC<PropTypes> = ({
   htmlFor,
   label,
   hidden,
   required,
-}) => (
-  <label
-    className={clsx({
-      'AriaHidden': hidden,
-      'Field__label': true,
-      [fieldLabelCID]: true,
-    })}
-    htmlFor={htmlFor}
-  >
-    {label}
-    {required && (
-      <span className="Field__label-required">
-        *
-      </span>
-    )}
-  </label>
-);
+}) => {
+  const classes = useStyles();
+
+  return (
+    <label
+      className={clsx({
+        'AriaHidden': hidden,
+        [classes.fieldLabel]: true,
+        [fieldLabelCID]: true,
+      })}
+      htmlFor={htmlFor}
+    >
+      {label}
+      {required && (
+        <span className="Field__label-required">
+          *
+        </span>
+      )}
+    </label>
+  );
+};
 
 export default FieldLabel;
