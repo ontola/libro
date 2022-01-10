943b7d7fe7d848b3828ba9d8feab36b6c2c6db04
Arthur Dingemans <arthur@argu.co>
2022-01-10 17:11:10 +0100

    core#954 Render typeform popup in our component tree
    The Material portal was taking away the focus

--- app/async/Typeform/index.tsx
+++ app/async/Typeform/index.tsx
@@ -6,18 +6,23 @@ import { TypeformProps } from '../../containers/Typeform';
 const Typeform = ({
   url,
   ...popupOpts
-}: TypeformProps): null => {
+}: TypeformProps): JSX.Element => {
+  const ref = React.useRef<HTMLDivElement>(null);
+
   React.useEffect(() => {
-    const popup = makePopup(
-      url,
-      popupOpts,
-    );
-    popup.open();
+    if (ref.current) {
+      const popup = makePopup(
+        url,
+        {
+          container: ref.current,
+          ...popupOpts,
+        },
+      );
+      popup.open();
+    }
+  }, [ref.current, url]);
 
-    return () => popup.close();
-  }, [url]);
-
-  return null;
+  return <div ref={ref} />;
 };
 
 export default Typeform;
