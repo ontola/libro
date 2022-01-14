bd09b3dc41905f223813b2081ade43f557a91277
Arthur Dingemans <arthur@argu.co>
2022-01-14 11:06:12 +0100

    core#954 Fix typeform on large windows

--- app/async/Typeform/index.tsx
+++ app/async/Typeform/index.tsx
@@ -1,12 +1,24 @@
+import { makeStyles } from '@material-ui/core/styles';
 import { makePopup } from '@typeform/embed';
 import React from 'react';
 
 import { TypeformProps } from '../../containers/Typeform';
 
+const useStyles = makeStyles(() => ({
+  wrapper: {
+    bottom: 0,
+    left: 0,
+    position: 'fixed',
+    right: 0,
+    top: 0,
+  },
+}));
+
 const Typeform = ({
   url,
   ...popupOpts
 }: TypeformProps): JSX.Element => {
+  const classes = useStyles();
   const ref = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
@@ -22,7 +34,11 @@ const Typeform = ({
     }
   }, [ref.current, url]);
 
-  return <div ref={ref} />;
+  return (
+    <div className={classes.wrapper}>
+      <div ref={ref} />
+    </div>
+  );
 };
 
 export default Typeform;
