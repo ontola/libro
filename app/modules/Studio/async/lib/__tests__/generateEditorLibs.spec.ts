2e5f850657c0fc7f5a7ae99c50f80e51869db3d6
Polle Pas <polle@argu.co>
2022-03-01 12:11:14 +0100

    core#822 Editor intelisense

---
+++ app/modules/Studio/async/lib/__tests__/generateEditorLibs.spec.ts
@@ -0,0 +1,40 @@
+import { createNS } from '@ontologies/core';
+
+import { generateEditorLibs } from '../generateEditorLibs/index';
+import { OntologyMap, generateOntologyDeclerations } from '../generateEditorLibs/generateOntologyDeclerations';
+
+const ns = createNS('https://fuckblockchain.com/ns#');
+
+const fakeOntology = {
+  ns,
+  // eslint-disable-next-line sort-keys
+  TrendGraph: ns('TrendGraph'),
+  ['Weird:Variable//Thing']: ns('Weird:Variable//Thing'),
+  trend: ns('trend'),
+};
+
+const ontologyMap: OntologyMap = [
+  [fakeOntology, 'fakeOntology'],
+];
+
+describe('generateEditorLibs', () => {
+  it('should return a string', () => {
+    const result = generateEditorLibs();
+    expect(typeof result).toBe('string');
+  });
+
+  describe('generateOntologyDeclerations', () => {
+    it('should return a string', () => {
+      const result = generateOntologyDeclerations([]);
+      const result2 = generateOntologyDeclerations(ontologyMap);
+
+      expect(typeof result).toEqual('string');
+      expect(typeof result2).toEqual('string');
+    });
+
+    it('should parse the ontology correctly', () => {
+      const result = generateOntologyDeclerations(ontologyMap);
+      expect(result).toMatchSnapshot();
+    });
+  });
+});
