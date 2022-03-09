b4ecd5c32bcee1dab52961a88d51a0585e193236
Arthur Dingemans <arthur@argu.co>
2022-03-09 12:23:26 +0100

    dexpods#143 Add dexes logo

--- app/themes/dexes/header/index.tsx
+++ app/themes/dexes/header/index.tsx
@@ -1,11 +1,22 @@
 import { Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import NavBarContent from '../../../components/NavBarContent';
+import retrievePath from '../../../helpers/iris';
+import { frontendIRI } from '../../../ontology/app';
 import Navbar from '../../../topologies/Navbar';
+import { LibroTheme } from '../../themes';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles<LibroTheme>((theme) => ({
+  logoWrapper: {
+    '& img': {
+      maxHeight: '100%',
+    },
+    display: 'inline-block',
+    height: theme.appBar.height,
+  },
   wrapper: {
     marginTop: '1em',
   },
@@ -13,10 +24,24 @@ const useStyles = makeStyles(() => ({
 
 const DexesHeader = (): JSX.Element => {
   const classes = useStyles();
+  const [logo, setLogo] = React.useState<string | undefined>();
+  React.useEffect(() => {
+    // eslint-disable-next-line no-inline-comments
+    import(/* webpackChunkName: "DutchGovernmentTheme" */ './Logo').then((encoded) => setLogo(encoded.default));
+  });
 
   return (
     <div className={classes.wrapper}>
       <Container maxWidth="xl">
+        <NavLink
+          className={classes.logoWrapper}
+          to={retrievePath(frontendIRI)!}
+        >
+          <img
+            alt="Dexes logo"
+            src={logo}
+          />
+        </NavLink>
         <Navbar
           float
         >
