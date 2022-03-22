866f3572fa04b5e9df874188da91196a9cc1e304
Polle Pas <polle@argu.co>
2022-03-22 17:35:11 +0100

    core#12 Hotfix Elements

--- app/views/RichText/Document.tsx
+++ app/views/RichText/Document.tsx
@@ -1,15 +1,15 @@
 import {
   FC,
+  Property,
   register,
 } from 'link-redux';
 import React from 'react';
 
-import AllWithProperty from '../../components/AllWithProperty';
 import elements from '../../ontology/elements';
 import { allTopologies } from '../../topologies';
 
 const Document: FC = () => (
-  <AllWithProperty label={elements.children} />
+  <Property label={elements.children} />
 );
 
 Document.type = elements.Document;
