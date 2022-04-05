fb4ecbe22c0f9f0bbda60656fc899f4804d9c4ed
Job Eijdems <job@argu.co>
2022-04-05 16:42:11 +0200

    core#1138 Implement PageHeader redesign

--- app/topologies/PageHeader/index.tsx
+++ app/topologies/PageHeader/index.tsx
@@ -16,7 +16,6 @@ import {
   Margin,
   Size, 
 } from '../../themes/themes';
-import { cardClassIdentifier } from '../Card/sharedCardStyles';
 import Container from '../Container';
 
 export { default as PageHeaderImage } from './PageHeaderImage';
@@ -27,8 +26,6 @@ export { default as PageHeaderText } from './PageHeaderText';
 export const pageHeaderTopology = argu.ns('pageHeader');
 
 const defaultPercentage = 50;
-
-const pageHeaderInnerCID = 'CID-PageHeaderInner';
 
 type PageHeaderProps = WithStyles<typeof styles> & {
   // URL to the background image
@@ -43,37 +40,27 @@ const styles = (theme: LibroTheme) => createStyles({
       bottom: 0,
       height: '100%',
     },
-    [theme.breakpoints.up('lg')]: {
-      height: '15em',
-    },
-    [`& .${cardClassIdentifier}`]: {
-      bottom: 0,
-      maxWidth: '35em',
-      position: 'absolute',
-      width: 'calc(100% - 1.5rem)',
-    },
-    background: 'var(--navbar-background)',
-    height: '10em',
     marginBottom: theme.spacing(Margin.Large),
     marginTop: `-${theme.spacing(Margin.Medium)}`,
-    position: 'relative',
   },
 
   pageHeaderBackground: {
-    [`& .${pageHeaderInnerCID}`]: {
-      backgroundPositionX: 'center',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-    },
+    backgroundPositionX: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
     height: '20em',
     [theme.breakpoints.up('lg')]: {
       height: '35em',
     },
   },
 
-  pageHeaderInner: {
-    backgroundColor: theme.palette.transparent.light7,
-    height: '100%',
+  pageHeaderNoBackground: {
+    backgroundColor: 'var(--navbar-background)',
+    height: '10em',
+    opacity: 0.3,
+    [theme.breakpoints.up('lg')]: {
+      height: '15em',
+    },
   },
 });
 
@@ -95,30 +82,28 @@ class PageHeader extends TopologyProvider<PageHeaderProps> {
     const className = clsx({
       [this.props.classes.pageHeader]: true,
       [this.props.classes.pageHeaderBackground]: this.props.background,
+      [this.props.classes.pageHeaderNoBackground]: !this.props.background,
     });
 
     if (this.props.background) {
       style.backgroundImage = `url(${this.props.background})`;
-
       style.backgroundPositionY = `${this.props.positionY || defaultPercentage}%`;
     }
 
     return (
-      <div className={className}>
+      <React.Fragment>
         <div
-          className={clsx(
-            pageHeaderInnerCID,
-            this.props.classes.pageHeaderInner,
-          )}
+          className={className}
           style={style}
+        />
+        <Container
+          size={Size.Large}
         >
-          <Container size={Size.Large}>
-            {this.wrap((
-              this.props.children || <Type />
-            ))}
-          </Container>
-        </div>
-      </div>
+          {this.wrap((
+            this.props.children || <Type />
+          ))}
+        </Container>
+      </React.Fragment>
     );
   }
 }
--- app/views/Thing/ThingPageHeader.tsx
+++ app/views/Thing/ThingPageHeader.tsx
@@ -1,12 +1,15 @@
+import { makeStyles } from '@material-ui/core';
 import * as schema from '@ontologies/schema';
 import {
   FC,
   Property,
   register,
+  useStrings,
 } from 'link-redux';
 import React from 'react';
 
 import CardContent from '../../components/Card/CardContent';
+import CollapseText from '../../components/CollapseText';
 import HeaderWithMenu from '../../components/HeaderWithMenu';
 import argu from '../../ontology/argu';
 import { CardMain } from '../../topologies/Card';
@@ -18,28 +21,48 @@ import {
 } from '../../topologies/PageHeader';
 import { defaultMenus } from '../common';
 
-const ThingPageHeader: FC = ({ subject }) => (
-  <div
-    about={subject?.value}
-    className="ThingPageHeader"
-  >
-    <CardMain>
-      <CardContent>
-        <PageHeaderImageAndTextWrapper>
-          <PageHeaderText>
-            <HeaderWithMenu menu={defaultMenus}>
-              <Property label={schema.name} />
-            </HeaderWithMenu>
-            <ContentDetails>
-              <Property label={argu.grantedGroups} />
-            </ContentDetails>
-            <Property label={[schema.description, schema.text]} />
-          </PageHeaderText>
-        </PageHeaderImageAndTextWrapper>
-      </CardContent>
-    </CardMain>
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  thingPageHeader: {
+    marginTop: '-5rem',
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+    maxWidth: theme.breakpoints.values.xl * 2 / 3,
+    [theme.breakpoints.up('lg')]: {
+      paddingRight: '1.125rem',
+    },
+  },
+}));
+
+const ThingPageHeader: FC = ({ subject }) => {
+  const classes = useStyles();
+  const [text] = useStrings([schema.text, schema.description]);
+
+  return (
+    <div
+      about={subject?.value}
+      className={classes.thingPageHeader}
+    >
+      <CardMain>
+        <CardContent>
+          <PageHeaderImageAndTextWrapper>
+            <PageHeaderText>
+              <HeaderWithMenu menu={defaultMenus}>
+                <Property label={schema.name} />
+              </HeaderWithMenu>
+              <ContentDetails>
+                <Property label={argu.grantedGroups} />
+              </ContentDetails>
+              <CollapseText
+                id={subject.value}
+                minCharacters={700}
+                text={text ?? ''}
+              />
+            </PageHeaderText>
+          </PageHeaderImageAndTextWrapper>
+        </CardContent>
+      </CardMain>
+    </div>
+  );
+};
 
 ThingPageHeader.type = schema.Thing;
 
