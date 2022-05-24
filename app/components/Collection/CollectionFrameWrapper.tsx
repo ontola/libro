b408ce2bd9c5564f995d30c84ee7ca7c5560fc9c
Job Eijdems <job@argu.co>
2022-05-24 14:29:59 +0200

    core#1079 Make Collection HeaderFloat always visible

--- app/components/Collection/CollectionFrameWrapper.tsx
+++ app/components/Collection/CollectionFrameWrapper.tsx
@@ -1,32 +1,15 @@
-import { makeStyles } from '@mui/styles';
 import React from 'react';
 
 import HeadingContext from '../Heading/HeadingContext';
-
-import { CollectionHeaderFloatCID } from './HeaderFloat';
-
-const useStyles = makeStyles({
-  wrapper: {
-    [`&:hover .${CollectionHeaderFloatCID}, &:focus-within .${CollectionHeaderFloatCID}`]: {
-      opacity: 1,
-    },
-  },
-});
 
 export interface CollectionFrameWrapperProps {
   Wrapper: React.ElementType,
 }
 
-export const CollectionFrameWrapper = ({ Wrapper, children }: React.PropsWithChildren<CollectionFrameWrapperProps>): JSX.Element => {
-  const classes = useStyles();
-
-  return (
-    <Wrapper>
-      <HeadingContext>
-        <div className={classes.wrapper}>
-          {children}
-        </div>
-      </HeadingContext>
-    </Wrapper>
-  );
-};
+export const CollectionFrameWrapper = ({ Wrapper, children }: React.PropsWithChildren<CollectionFrameWrapperProps>): JSX.Element => (
+  <Wrapper>
+    <HeadingContext>
+      {children}
+    </HeadingContext>
+  </Wrapper>
+);
--- app/components/Collection/HeaderFloat.tsx
+++ app/components/Collection/HeaderFloat.tsx
@@ -1,79 +1,28 @@
-import {
-  ClickAwayListener,
-  IconButton,
-  useMediaQuery,
-  useTheme,
-} from '@mui/material';
-import makeStyles from '@mui/styles/makeStyles';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import clsx from 'clsx';
 import { Property, Resource } from 'link-redux';
 import React from 'react';
-import { useIntl } from 'react-intl';
 
 import ontola from '../../ontology/ontola';
-import { BreakPoints, LibroTheme } from '../../themes/themes';
-import { collectionMessages } from '../../translations/messages';
 
 import { useCollectionOptions } from './CollectionContext';
 import CollectionCreateButton from './CollectionCreateButton';
 import CollectionFilterToggle, { CollectionFilterProps } from './CollectionFilterToggle';
 
-export const CollectionHeaderFloatCID = 'CID-CollectionHeaderFloat';
-
-const useStyles = makeStyles<LibroTheme>((theme) => ({
-  headerFloat: {
-    [theme.breakpoints.up(BreakPoints.Medium)]: {
-      opacity: 0,
-      transition: 'opacity 150ms ease',
-    },
-  },
-}));
-
 export const HeaderFloat = ({
   filterContainerRef,
 }: CollectionFilterProps): JSX.Element => {
-  const classes = useStyles();
-  const intl = useIntl();
-
   const {
     hidePagination,
     originalCollection,
   } = useCollectionOptions();
   const renderPagination = !hidePagination;
-  const theme = useTheme();
-  const screenIsWide = useMediaQuery(theme.breakpoints.up(BreakPoints.Medium));
-
-  const [renderButtons, setRenderButtons] = React.useState(false);
-
-  const handleClickAway = React.useCallback(() => {
-    setRenderButtons(false);
-  }, [setRenderButtons]);
-
-  const showButtons = screenIsWide || renderButtons;
 
   return (
-    <ClickAwayListener onClickAway={handleClickAway}>
-      <span className={clsx(CollectionHeaderFloatCID, classes.headerFloat)}>
-        {showButtons && (
-          <React.Fragment>
-            {renderPagination && <CollectionFilterToggle filterContainerRef={filterContainerRef} />}
-            {renderPagination && <Property label={ontola.sortOptions} />}
-            <Resource subject={originalCollection}>
-              <CollectionCreateButton />
-            </Resource>
-          </React.Fragment>
-        )}
-        {!showButtons && (
-          <IconButton
-            aria-label={intl.formatMessage(collectionMessages.collectionActionsAriaLabel)}
-            size="small"
-            onClick={() => setRenderButtons(true)}
-          >
-            <MoreVertIcon />
-          </IconButton>
-        )}
-      </span>
-    </ClickAwayListener>
+    <span>
+      {renderPagination && <CollectionFilterToggle filterContainerRef={filterContainerRef} />}
+      {renderPagination && <Property label={ontola.sortOptions} />}
+      <Resource subject={originalCollection}>
+        <CollectionCreateButton />
+      </Resource>
+    </span>
   );
 };
