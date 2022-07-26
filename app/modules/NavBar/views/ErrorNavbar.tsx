4bc7bb011a6cedde8d580e73b94cdc311df8239e
Thom van Kalkeren <thom@argu.co>
2022-07-26 16:09:47 +0200

    core#3 Remove unreachable code

--- app/modules/NavBar/views/ErrorNavbar.tsx
+++ app/modules/NavBar/views/ErrorNavbar.tsx
@@ -1,17 +1,13 @@
 import { FC, register } from 'link-redux';
 import React from 'react';
-import FontAwesome from 'react-fontawesome';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import { errorMessages } from '../../../translations/messages';
-import BlurButton from '../../Common/components/BlurButton';
 import { bodyDescriptorForStatus, useErrorStatus } from '../../Common/components/Error/errorMessages';
 import { ErrorComponentProps } from '../../Common/components/Error/helpers';
 import useErrorReload from '../../Common/hooks/useErrorReload';
 import { ERROR_CLASSES } from '../../Common/lib/metaData';
 import { NavbarLinkLink } from '../components/NavbarLink';
-import NavbarLinkIcon from '../components/NavbarLink/NavbarLinkIcon';
-import NavbarLinkLabel from '../components/NavbarLink/NavbarLinkLabel';
 import { navbarTopology } from '../topologies/Navbar';
 
 const ErrorNavbar: FC<ErrorComponentProps> = (props) => {
@@ -44,30 +40,6 @@ const ErrorNavbar: FC<ErrorComponentProps> = (props) => {
       title={`${bodyText} ${retryText}`}
       onClick={reload}
     />
-  );
-
-  return (
-    <BlurButton
-      className="NavbarLink"
-      style={{
-        cursor: 'pointer',
-      }}
-      title={`${bodyText} ${retryText}`}
-      onClick={reload}
-    >
-      <NavbarLinkIcon>
-        <FontAwesome
-          name={loading ? 'spinner' : 'exclamation-triangle'}
-          spin={loading}
-        />
-      </NavbarLinkIcon>
-      <NavbarLinkLabel>
-        <FormattedMessage
-          defaultMessage="Retry"
-          id="https://app.argu.co/i18n/errors/inlineButton/label"
-        />
-      </NavbarLinkLabel>
-    </BlurButton>
   );
 };
 
