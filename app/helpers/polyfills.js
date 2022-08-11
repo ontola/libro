7dd97ae1f89bc1803c0fa006cbb5038f95950397
Thom van Kalkeren <thom@argu.co>
2022-08-11 13:05:55 +0200

    core#3 Remove hard polyfills
    They were very useful in 2019

--- app/helpers/polyfills.js
+++ app/helpers/polyfills.js
@@ -1,9 +1,3 @@
 /* eslint-disable global-require */
 /* global require */
-import '@formatjs/intl-relativetimeformat/polyfill';
-import 'core-js/features/array/flat';
-import 'core-js/features/array/flat-map';
-import 'core-js/features/array/from';
-import 'intl-pluralrules';
-
 require('smoothscroll-polyfill').polyfill();
