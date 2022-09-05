3d956bae1929abcebd7026525a558efb51af157d
Arthur Dingemans <arthur@argu.co>
2022-09-05 14:44:50 +0200

    chore: cleanup unused view

--- app/modules/Argu/views/GrantTree/index.ts
+++ app/modules/Argu/views/GrantTree/index.ts
@@ -1,9 +1,7 @@
 import NodeFull from './NodeFull';
-import PermissionGroupContainer from './PermissionGroupContainer';
 import PermissionTableCell from './properties/permissionTableCell';
 
 export default [
   ...NodeFull,
-  ...PermissionGroupContainer,
   ...PermissionTableCell,
 ];
